f83bd31f1af132842e58696dbb4f9beb2df87399
jesusdoza <jesus.doza@gmail.com>
2022-10-03 11:06:03 -0500

    add user role verification to delete

--- controllers/repair.js
+++ controllers/repair.js
@@ -24,10 +24,11 @@ module.exports.deletePost = async (req, res)=>{
             report.removed = true;
             await report.save()
             // res.send({message:'user is admin or creator',rep:report})
-            res.redirect('/repair/')
-            
-        }   
-        // res.send({user,report})
+            res.redirect('/repair/') 
+        }else{
+            console.log('user not allowed')
+            throw new Error(`user: ${user.username} not allowed`)
+        }
     } catch (error) {
         
         res.send({err:'delete error implemented ID: '+ req.params.id, message: error.message})
