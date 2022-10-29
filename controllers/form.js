52515c5a9c9c82c6a7cbe8ffca33b12b6ff3eabd
Jesus Mendoza <101015821+jesusdoza@users.noreply.github.com>
2022-10-28 21:09:21 -0500

    working

--- controllers/form.js
+++ controllers/form.js
@@ -1,30 +1,38 @@
 const signature = require('../modules/signuploadform')
 const router =  require('express').Router()
+const User = require('../models/User')
 
 
-module.exports={
-    getForm:async (req, res)=>{
-        res.render('repairform.ejs',{title:"Repair Submission",user:req.user});
-    },
+module.exports.getForm=async (req, res)=>{
+
+    const foundUser = await User.findById(req.user._id).lean()
+    console.log(foundUser)
+
+    res.render('repairform.ejs',{
+        title:"Repair Submission",
+        user:req.user,
+        groups:[]
+    });
+},
 
 
-    signForm:async (req, res)=>{
-        
-      //todo get signature and respond
-      const sig = signature.signuploadform();
-      console.log(`signform signature received `, sig)
-  
-      try{
-          res.status(200).json({
-              signature: sig.signature,
-              timestamp: sig.timestamp,
-              cloudname: process.env.cloud_name,
-              apikey: process.env.cloud_key,
-              folder:process.env.cloud_folder
-          })
-      }catch(error){
-      } 
-    }
 
+module.exports.signForm=async (req, res)=>{
+    
+    //todo get signature and respond
+    const sig = signature.signuploadform();
+    console.log(`signform signature received `, sig)
 
+    try{
+        res.status(200).json({
+            signature: sig.signature,
+            timestamp: sig.timestamp,
+            cloudname: process.env.cloud_name,
+            apikey: process.env.cloud_key,
+            folder:process.env.cloud_folder
+        })
+    }catch(error){
+    } 
 }
+
+
