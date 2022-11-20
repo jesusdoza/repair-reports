f7069aa637b6bd156b4ba63998650a8861530171
Jesus Mendoza <101015821+jesusdoza@users.noreply.github.com>
2022-11-19 19:34:39 -0600

    ADD cypress home page links tests

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,43 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("logout_mobile", () => {
+  ///logout
+  cy.get("[data-test='avatar-menu']").click();
+  cy.get("[data-test='avatar-menu'] li").should("be.visible");
+  cy.get("[data-test='avatar-menu'] li a[ href='/logout']").click();
+});
+
+Cypress.Commands.add("click_mobile_link", (link) => {
+  //test dashboard link
+  cy.get("[data-test='mobile-nav']").click();
+  cy.get(`[data-test='mobile-menu'] li [ href='${link}']`).click();
+  //if not logged in should direct to login
+  cy.location().should((loc) => {
+    expect(loc.href).to.eq("http://localhost:8000/login");
+  });
+});
+
+Cypress.Commands.add("mobile_login", (username, pass) => {
+  //login
+  cy.get("#email").type(username);
+  cy.get("#password").type(pass);
+  cy.get("form > .btn").click();
+});
+
+Cypress.Commands.add("test_mobile_home_links", (link, username, pass) => {
+  cy.click_mobile_link(`${link}`);
+  cy.location().should((loc) => {
+    expect(loc.href).to.eq("http://localhost:8000/login");
+  });
+
+  //login
+  cy.mobile_login(username, pass);
+  cy.location().should((loc) => {
+    expect(loc.href).to.eq(`http://localhost:8000${link}`);
+  });
+
+  //logout
+  cy.logout_mobile();
+});
