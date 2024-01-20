dcf2921e0601076e76f139d75101193539701e06
jesusdoza <jesus.doza@gmail.com>
2024-01-19 19:17:32 -0600

    modal

--- client/src/pages/LatestRepairsPage.tsx
+++ client/src/pages/LatestRepairsPage.tsx
@@ -2,7 +2,6 @@ import LoginModal from "../components/LoginSignUp/LoginModal";
 export default function LatestRepairs() {
   return (
     <div className="center-block">
-      <LoginModal />
       <h1>latest repairs</h1>
     </div>
   );
