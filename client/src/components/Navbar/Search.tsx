1541e538ab9cf5eaae2ea68b73768dab5773e351
jesusdoza <jesus.doza@gmail.com>
2024-01-24 09:46:50 -0600

    search component

--- client/src/components/Navbar/Search.tsx
+++ client/src/components/Navbar/Search.tsx
@@ -3,12 +3,13 @@ import React from "react";
 export default function Search() {
   const handleSearch = (event: React.SyntheticEvent) => {
     event.preventDefault();
-    console.log("event", event);
+    console.log("event", event.target.searchPhrase.value);
   };
   return (
     <form onSubmit={(event) => handleSearch(event)}>
       <input
         type="text"
+        name="searchPhrase"
         placeholder="Search"
         className="input input-bordered w-24 md:w-auto"
       />
