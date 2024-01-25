09ee7e994957081fbc0e8a48e5cbcd4f3dd2ac92
jesusdoza <jesus.doza@gmail.com>
2024-01-24 20:39:52 -0600

    add search functionality

--- client/src/components/Navbar/Search.tsx
+++ client/src/components/Navbar/Search.tsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import useRepairApi from "../../hooks/useRepairApi";
 
 export default function Search() {
-  const handleSearch = (event: React.SyntheticEvent) => {
+  const { searchForRepair } = useRepairApi();
+
+  const [search, setSearch] = useState("");
+  const handleSearch = async (event: React.SyntheticEvent) => {
     event.preventDefault();
-    console.log("event", event.target.searchPhrase.value);
+    console.log("search", search);
+    const results = await searchForRepair(search);
+    console.log("results", results);
   };
   return (
-    <form onSubmit={(event) => handleSearch(event)}>
+    <form onSubmit={handleSearch}>
       <input
+        onChange={(e) => setSearch(e.target.value)}
         type="text"
         name="searchPhrase"
         placeholder="Search"
