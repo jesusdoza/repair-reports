6bcbee3fd6eff509285f6d7aa20205535bd09e0e
jesusdoza <jesus.doza@gmail.com>
2024-02-12 19:19:28 -0600

    move to ejs

--- backend/routes/react/index.js
+++ backend/routes/react/index.js
@@ -3,14 +3,14 @@ const reactController = require("../../controllers/react");
 
 const path = require("path");
 
-//! NOT USED
 router.get("*", (req, res) => {
   // res.send({ serveApp: "serve app" });
-  console.log(
-    '(path.resolve("./public/react/index.html")',
-    path.resolve("./public/react/index.html")
-  );
-  res.sendFile(path.resolve("./public/react/index.html"));
+  // console.log(
+  //   '(path.resolve("./public/react/index.html")',
+  //   path.resolve("./public/react/index.html")
+  // );
+  // res.sendFile(path.resolve("./public/react/index.html"));
+  res.render("react.ejs");
 });
 
 module.exports = router;
