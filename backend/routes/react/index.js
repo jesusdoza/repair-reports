bb1fef84909b78a366868154b1a145116e88bda5
jesusdoza <jesus.doza@gmail.com>
2024-02-12 19:51:16 -0600

    print file

--- backend/routes/react/index.js
+++ backend/routes/react/index.js
@@ -4,6 +4,21 @@ const reactController = require("../../controllers/react");
 const path = require("path");
 
 router.get("*", (req, res) => {
+  const directoryPath = "../backend/public"; // Replace this with your directory path
+
+  // Read the contents of the directory
+  fs.readdir(directoryPath, (err, files) => {
+    if (err) {
+      console.error("Error reading directory:", err);
+      return;
+    }
+
+    // Print the names of files and subdirectories
+    files.forEach((file) => {
+      console.log(file);
+    });
+  });
+
   res.sendFile(path.resolve("./public/react"));
 });
 
