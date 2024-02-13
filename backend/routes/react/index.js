b6565827afa87dc9cc16da8a00ebd60345ab42d7
jesusdoza <jesus.doza@gmail.com>
2024-02-12 19:44:38 -0600

    react/dir

--- backend/routes/react/index.js
+++ backend/routes/react/index.js
@@ -4,7 +4,7 @@ const reactController = require("../../controllers/react");
 const path = require("path");
 
 router.get("*", (req, res) => {
-  res.sendFile(path.resolve("./public/images/no-image.png"));
+  res.sendFile(path.resolve("./public/react"));
 });
 
 module.exports = router;
