ef67512a093613bdad44acfd196532396c8d1ff6
jesusdoza <jesus.doza@gmail.com>
2024-02-12 18:24:46 -0600

    serve react app

--- backend/controllers/react/index.js
+++ backend/controllers/react/index.js
@@ -1,5 +1,6 @@
 const serveApp = (req, res) => {
-  res.send({ serveApp: "serve app" });
+  // res.send({ serveApp: "serve app" });
+  res.sendFile(path.resolve("./public/react/index.html"));
 };
 
 module.exports = { serveApp };
