49dda5ef3b050ff50a49a31786185fb2f2155384
jesus doza <jesus.doza@gmail.com>
2024-02-15 19:40:54 -0600

    add availableOptions components

--- client/src/pages/EditRepairPage.tsx
+++ client/src/pages/EditRepairPage.tsx
@@ -1,7 +1,8 @@
 import { useLocation } from "react-router-dom";
 import { repairDataT } from "../hooks/useGetLatest";
-import { useEffect, useState } from "react";
+import { FormEventHandler, useEffect, useState } from "react";
 import EditProcedureList from "../components/RepairEdit/EditProcedureList";
+import { AvailableEnginesSelect } from "../components/AvailableOptions/AvailableEngines";
 
 export default function EditRepairPage() {
   const { state: data }: { state: repairDataT } = useLocation();
@@ -11,11 +12,18 @@ export default function EditRepairPage() {
     console.log("new data", newData);
   }, [newData]);
 
+  const handleUpdate = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formdata = event.currentTarget;
+    console.log("formdata", formdata);
+  };
+
   return (
-    <section>
+    <form onSubmit={handleUpdate}>
       <legend className=" border-4 rounded-lg p-2 border-gray-600">
-        <h1 className=" text-4xl">{newData.title ? newData.title : ""}</h1>
+        <span className=" text-4xl">Title:</span>
         <input
+          className="text-4xl"
           id="title"
           name="title"
           type="text"
@@ -30,76 +38,7 @@ export default function EditRepairPage() {
           <span>created by user:</span>
           <div className="badge badge-neutral"> {newData.createdBy}</div>
         </div>
-        <div>
-          <span>engine make:</span>
-          <div className="badge badge-neutral"> {newData.engineMake}</div>
-          {/*//todo input for enginemake */}
-
-          <section className="container blue-grey darken-1">
-            <fieldset className="border engine-models">
-              <legend>Choose Engine</legend>
-              <span>
-                <label htmlFor="cat">
-                  <div className="badge badge-accent">
-                    <input
-                      id="cat"
-                      name="model"
-                      value="cat"
-                      type="radio"
-                      className=" model radio"
-                    />
-                    Caterpillar
-                  </div>
-                </label>
-              </span>
-              <span>
-                <label htmlFor="detroit">
-                  <div className="badge badge-accent">
-                    <input
-                      id="detroit"
-                      name="model"
-                      value="detroit"
-                      type="radio"
-                      className=" model radio"
-                    />
-                    Detroit
-                  </div>
-                </label>
-              </span>
-
-              <span>
-                <label htmlFor="cummins">
-                  <div className="badge badge-accent">
-                    <input
-                      id="cummins"
-                      name="model"
-                      value="cummins"
-                      type="radio"
-                      className=" model radio"
-                    />
-                    <span>Cummins</span>
-                  </div>
-                </label>
-              </span>
-
-              <span>
-                <label htmlFor="other">
-                  <div className="badge badge-accent">
-                    <input
-                      id="other"
-                      name="model"
-                      value="other"
-                      type="radio"
-                      className=" model radio"
-                    />
-                    <span>Other</span>
-                  </div>
-                </label>
-              </span>
-            </fieldset>
-          </section>
-          {/* end */}
-        </div>
+        <AvailableEnginesSelect defaultValue={newData.engineMake} />
         <div>
           <span>user group:</span>
           <div className="badge badge-neutral"> {newData.group}</div>
@@ -113,7 +52,7 @@ export default function EditRepairPage() {
         <h3 className="text-xl">Repair procedures</h3>
         <EditProcedureList list={newData.procedureArr} />
       </section>
-      {/* if user id matches created by field user can use edit tools */}
-    </section>
+      <button className="btn">Update</button>
+    </form>
   );
 }
