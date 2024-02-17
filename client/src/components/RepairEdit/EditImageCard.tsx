b481cd7489c37f8dc25ac13eeffb9643260e190d
jesus doza <jesus.doza@gmail.com>
2024-02-17 13:51:46 -0600

    add camera button and preview

--- client/src/components/RepairEdit/EditImageCard.tsx
+++ client/src/components/RepairEdit/EditImageCard.tsx
@@ -1,5 +1,4 @@
-// @ts-expect-error 'react is used'
-import React from "react";
+import React, { useState, useRef } from "react";
 
 export function EditImageCard({
   url,
@@ -8,34 +7,89 @@ export function EditImageCard({
   url: string;
   setUrl: (newUrl: string) => void;
 }) {
-  console.log("setUrl", setUrl);
+  const [imagePreview, setImagePreview] = useState<string | ArrayBuffer | null>(
+    url
+  );
+
+  const [activeCamera, setActiveCamera] = useState(false);
+
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files && event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImagePreview(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const handleCapture = async () => {
+    setActiveCamera((state) => !state);
+    try {
+      const mediaStream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+      });
+      if (videoRef.current) {
+        videoRef.current.srcObject = mediaStream;
+      }
+    } catch (error) {
+      console.error("Error accessing camera:", error);
+    }
+  };
+
+  const captureFrame = () => {
+    const canvas = document.createElement("canvas");
+    if (videoRef.current) {
+      canvas.width = videoRef.current.videoWidth;
+      canvas.height = videoRef.current.videoHeight;
+      canvas.getContext("2d")?.drawImage(videoRef.current, 0, 0);
+      const dataUrl = canvas.toDataURL("image/png");
+      setImagePreview(dataUrl);
+    }
+  };
+
   return (
     <div
       key={url}
-      className="w-3/4 bg-green-400">
+      className="w-3/4 bg-green-100">
       <div className="flex flex-col">
-        <img
-          src={url}
-          className="rounded-box "
-        />
-        <label
-          htmlFor=""
-          className="">
-          <h3>original url</h3>
-          <input
-            type="text"
-            disabled
-            value={url}
+        {imagePreview && (
+          <img
+            src={imagePreview.toString()}
+            alt="Preview"
+            style={{ maxWidth: "100%" }}
           />
-        </label>
+        )}
         <label
           htmlFor=""
           className="text-black border-2 border-s-violet-100">
-          <h3>Replace image</h3>
+          <h3 className=" bg-gray-700">Change image</h3>
+          {activeCamera && (
+            <video
+              ref={videoRef}
+              style={{ display: "block", maxWidth: "100%" }}
+              autoPlay
+            />
+          )}
           <input
             type="file"
             accept="image/*"
+            onChange={handleFileChange}
           />
+          <div
+            className="btn"
+            onClick={captureFrame}>
+            capture
+          </div>
+
+          <div
+            className="btn"
+            onClick={handleCapture}>
+            Use camera
+          </div>
         </label>
       </div>
     </div>
