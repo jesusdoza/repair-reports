b8afada54b45d01a7b9a726ba169ff295f434a98
jesus doza <jesus.doza@gmail.com>
2024-02-26 20:24:28 -0600

    update availableOptions component

--- client/src/components/AvailableOptions/AvailableOptions.tsx
+++ client/src/components/AvailableOptions/AvailableOptions.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { SetStateAction } from "react";
+import { repairDataT } from "../../hooks/useGetLatest";
 
 type selectionT = {
   value: string;
@@ -9,10 +10,12 @@ export default function AvailableOptions({
   options,
   name,
   title,
+  callback,
 }: {
   options: selectionT[];
   name: string;
   title: string;
+  callback?: (engineSelected: string) => void;
 }) {
   const selections = options.map((item: selectionT) => {
     return createOption(item);
@@ -21,6 +24,11 @@ export default function AvailableOptions({
     <div className="flex gap-3">
       <span>{title}</span>
       <select
+        onChange={(event) => {
+          console.log("event select", event.target.value);
+
+          if (callback) callback(event.target.value || options[0].value);
+        }}
         name={name}
         className="select select-info w-full max-w-xs">
         {selections}
