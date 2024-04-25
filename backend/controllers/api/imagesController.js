555a2de87d5b65444ccb984526caa398448f23e5
jesusdoza <jesus.doza@gmail.com>
2024-04-24 22:24:11 -0500

    add signature utility to images controller

--- backend/controllers/api/imagesController.js
+++ backend/controllers/api/imagesController.js
@@ -1,4 +1,6 @@
 const axios = require("axios").default;
+const cloudinary = require("cloudinary").v2;
+const signatureUtility = require("../../utilities/signuploadform");
 
 const router = require("express").Router();
 
@@ -6,21 +8,26 @@ const CLOUD_NAME = process.env.CLOUD_NAME;
 const RESOURCE_TYPE = "image";
 const API_URL = `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/${RESOURCE_TYPE}/destroy`;
 
-module.exports = {
-  deleteImage: async (req, res) => {
-    const { imageId } = req.body;
+const deleteImage = async (req, res) => {
+  const { imageId } = req.body;
+  const signature = signatureUtility.signuploadform();
 
-    //TODO get secrets and signature
-    const body = { signature, api_key, timestamp, public_id };
+  //TODO get secrets and signature
+  const body = { signature, api_key, timestamp, public_id };
 
-    const response = await axios.post(API_URL, { body });
+  const response = await cloudinary.v2.uploader.destroy(imageId, {
+    signature,
+  });
 
-    //TODO do something with cloud response
-    console.log("response", response);
+  // const response = await axios.post(API_URL, { body });
 
-    res.send({
-      route: "images delete",
-      body: req.body,
-    });
-  },
+  //TODO do something with cloud response
+  console.log("response", response);
+
+  res.send({
+    route: "images delete",
+    body: req.body,
+  });
 };
+
+module.exports = { deleteImage };
