6df9c0482e0b010c35e4e597b2bc23c41c29ad8a
jesus doza <jesus.doza@gmail.com>
2024-05-02 10:32:48 -0500

    add cleanup script for node modules on client directory

---
+++ cleanupClient.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+const path = require("path");
+
+// function removeDirectory(dirPath) {
+//   fs.rmSync(dirPath, { recursive: true, force: true });
+//   console.log("done removing", dirPath);
+// }
+function removeDirectory(dirPath) {
+  if (fs.existsSync(dirPath)) {
+    fs.readdirSync(dirPath).forEach((file, index) => {
+      const filePath = path.join(dirPath, file);
+      if (fs.lstatSync(filePath).isDirectory()) {
+        try {
+          // Recursive call for directories
+          removeDirectory(filePath);
+        } catch (error) {
+          console.log("failed to delete directory: ", filePath);
+        }
+      } else {
+        // Delete file
+        try {
+          fs.unlinkSync(filePath);
+        } catch (error) {
+          console.log("failed to delete file: ", filePath);
+        }
+      }
+    });
+
+    try {
+    } catch (error) {}
+    // Remove the empty directory itself
+    fs.rmdirSync(dirPath);
+    console.log(`Directory ${dirPath} removed successfully.`);
+  } else {
+    console.log(`Directory ${dirPath} does not exist.`);
+  }
+}
+
+// Example usage
+// const directoryToRemove = "/path/to/your/directory";const DIRPATH = path.normalize("./client");
+const directoryToRemove = path.normalize("./client/node_modules");
+removeDirectory(directoryToRemove);
