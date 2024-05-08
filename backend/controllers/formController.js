334261cfbf40ddccc18c8a1c48e3013d95f67613
jesus doza <jesus.doza@gmail.com>
2024-05-08 10:04:06 -0500

    add POST signform to include req.body

--- backend/controllers/formController.js
+++ backend/controllers/formController.js
@@ -5,7 +5,6 @@ const mongoose = require("mongoose");
 
 const getForm = async (req, res) => {
   const foundUser = await User.findById(req.user._id);
-  console.log("found user from database", foundUser);
 
   if (!foundUser.groups) {
     //correction if user was an old version without groups
@@ -13,7 +12,6 @@ const getForm = async (req, res) => {
     await foundUser.save();
   }
   const userGroups = foundUser.groups;
-  console.log("userGroups", userGroups); //FIXME remove console.log
 
   res.render("repairform.ejs", {
     title: "Repair Submission",
@@ -22,6 +20,38 @@ const getForm = async (req, res) => {
   });
 };
 
+const postSignForm = async (req, res) => {
+  const userId = req.user._id;
+  console.log("req.body sign form", req.body);
+
+  const enviroment = process.env.NODE_ENV;
+
+  //requested folder user wants to upload to
+  //todo folder validation. Does user have auth to save in this folder maybe its a company folder
+  let desiredFolder = `${req.body?.folder ? req.body.folder : "no_folder"}`;
+
+  if (enviroment == "development") {
+    desiredFolder = "testfolder/" + desiredFolder;
+  }
+
+  //folder to organize this image in
+  //composed of folder requested by user and the userId
+  const imageFolder = `${desiredFolder}/${userId}`;
+
+  const sig = signature.signuploadform(imageFolder);
+
+  console.log(`signform signature received `, sig);
+
+  try {
+    res.status(200).json({
+      signature: sig.signature,
+      timestamp: sig.timestamp,
+      cloudname: process.env.cloud_name,
+      apikey: process.env.cloud_key,
+      folder: imageFolder,
+    });
+  } catch (error) {}
+};
 const signForm = async (req, res) => {
   const userId = req.user._id;
 
@@ -48,4 +78,4 @@ const signForm = async (req, res) => {
   } catch (error) {}
 };
 
-module.exports = { signForm, getForm };
+module.exports = { signForm, getForm, postSignForm };
