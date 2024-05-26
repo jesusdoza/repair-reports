386e7adc31c49604d1d74b9f6aebbf2914b4bd5e
jesus doza <jesus.doza@gmail.com>
2024-05-26 14:08:30 -0500

    fix Repair class constructor

--- client/src/classes/Repair.ts
+++ client/src/classes/Repair.ts
@@ -1,4 +1,4 @@
-import { RepairDataT } from "../../types";
+import { ProcedureT, RepairDataT } from "../../types";
 import { Procedure } from "./Procedure";
 
 export class Repair {
@@ -12,22 +12,25 @@ export class Repair {
   constructor(repair: RepairDataT);
   constructor();
   constructor(repair?: RepairDataT) {
-
-    if(repair){
-      this.searchTags =repair?.searchTags  ? repair. : this.searchTags 
-      this.boardType = repair?.boardType  ? repair.boardType : this.boardType
-      this.engineMake = repair?.engineMake  ? repair.engineMake : this.engineMake;
-      this.group = repair?.group  ? repair.group : this.group
-      this.procedureArr= repair?.procedureArr  ? this.intializeProcedures(repair.procedureArr) : this.procedureArr
-      this.title = repair?.title  ? repair.title : this.title
+    if (repair) {
+      this.searchTags = repair?.searchTags
+        ? repair.searchTags
+        : this.searchTags;
+      this.boardType = repair?.boardType ? repair.boardType : this.boardType;
+      this.engineMake = repair?.engineMake
+        ? repair.engineMake
+        : this.engineMake;
+      this.group = repair?.group ? repair.group : this.group;
+      this.procedureArr = repair?.procedureArr
+        ? this.intializeProcedures(repair.procedureArr)
+        : this.procedureArr;
+      this.title = repair?.title ? repair.title : this.title;
     }
   }
 
-
-  intializeProcedures(procedures: ProcedureT[]):Procedure[]{
-
-    return procedures.map((proc)=>{
+  intializeProcedures(procedures: ProcedureT[]): Procedure[] {
+    return procedures.map((proc) => {
       return new Procedure(proc);
-    })
+    });
   }
 }
