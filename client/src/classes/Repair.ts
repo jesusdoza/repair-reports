f94a15f5ea71aa54d7fee785604e668d1ed0a80e
jesus doza <jesus.doza@gmail.com>
2024-05-26 13:49:48 -0500

    add argument contructor to Repair class

--- client/src/classes/Repair.ts
+++ client/src/classes/Repair.ts
@@ -1,3 +1,4 @@
+import { RepairDataT } from "../../types";
 import { Procedure } from "./Procedure";
 
 export class Repair {
@@ -8,5 +9,25 @@ export class Repair {
   public procedureArr: Procedure[] = [new Procedure()];
   public title = "New Repair";
 
-  constructor() {}
+  constructor(repair: RepairDataT);
+  constructor();
+  constructor(repair?: RepairDataT) {
+
+    if(repair){
+      this.searchTags =repair?.searchTags  ? repair. : this.searchTags 
+      this.boardType = repair?.boardType  ? repair.boardType : this.boardType
+      this.engineMake = repair?.engineMake  ? repair.engineMake : this.engineMake;
+      this.group = repair?.group  ? repair.group : this.group
+      this.procedureArr= repair?.procedureArr  ? this.intializeProcedures(repair.procedureArr) : this.procedureArr
+      this.title = repair?.title  ? repair.title : this.title
+    }
+  }
+
+
+  intializeProcedures(procedures: ProcedureT[]):Procedure[]{
+
+    return procedures.map((proc)=>{
+      return new Procedure(proc);
+    })
+  }
 }
