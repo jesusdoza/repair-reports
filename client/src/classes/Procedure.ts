f14fdd6d30ecad8a8ce722e6a69c8827db840730
jesus doza <jesus.doza@gmail.com>
2024-05-26 13:57:00 -0500

    add multi constructor to ProcedureClass

--- client/src/classes/Procedure.ts
+++ client/src/classes/Procedure.ts
@@ -1,15 +1,39 @@
 import { v4 as uuid4 } from "uuid";
-import { ProcedureT } from "../../types";
+import { ImageObjT, ProcedureT } from "../../types";
 import { ImageObj } from "./ImageObj";
 
 export class Procedure implements ProcedureT {
   public images = [""];
   public imageObjs: ImageObj[] = [new ImageObj()];
-  public imagesIdArr = [];
+  public imagesIdArr: string[] = [];
   public instructions = "";
   public procedureNum = 0;
-  public thumbs = [];
+  public thumbs: string[] = [];
   public _id = uuid4();
 
-  constructor() {}
+  constructor();
+  constructor(procedure: ProcedureT);
+  constructor(procedure?: ProcedureT) {
+    if (procedure) {
+      this.images = procedure?.images ? procedure.images : this.images;
+      this.imageObjs = procedure?.imageObjs
+        ? this.intializeImageObjs(procedure.imageObjs)
+        : this.imageObjs;
+      this.imagesIdArr = procedure?.imagesIdArr
+        ? procedure.imagesIdArr
+        : this.imagesIdArr;
+      this.instructions = procedure?.instructions
+        ? procedure.instructions
+        : this.instructions;
+      this.procedureNum = procedure?.procedureNum
+        ? procedure.procedureNum
+        : this.procedureNum;
+      this.thumbs = procedure?.thumbs ? procedure.thumbs : this.thumbs;
+      this._id = procedure?._id ? procedure._id : this._id;
+    }
+  }
+
+  intializeImageObjs(imageData: ImageObjT[]): ImageObj[] {
+    return;
+  }
 }
