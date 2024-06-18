5e29c13e0063f7830af75c5287f60dee7f17830e
jesusdoza <jesus.doza@gmail.com>
2024-06-18 08:26:44 -0500

    fix repairs/user route method

--- backend/routes/api/repairs.js
+++ backend/routes/api/repairs.js
@@ -4,7 +4,7 @@ const apiController = require("../../controllers/api/repairsController");
 router.get("/", apiController.getNewestRepairs);
 router.post("/", apiController.addRepair);
 router.put("/", apiController.updateRepair);
-router.get("/user", apiController.getNewestRepairs);
+router.get("/user", apiController.getRepairsforUser);
 router.get("/:id", apiController.getRepairById);
 
 module.exports = router;
