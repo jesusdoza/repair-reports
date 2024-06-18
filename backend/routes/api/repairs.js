2f89657bb6a2813207c0ba79c328f6aad7376fd9
jesusdoza <jesus.doza@gmail.com>
2024-06-18 07:54:00 -0500

    add new repair/user route GET

--- backend/routes/api/repairs.js
+++ backend/routes/api/repairs.js
@@ -4,6 +4,7 @@ const apiController = require("../../controllers/api/repairsController");
 router.get("/", apiController.getNewestRepairs);
 router.post("/", apiController.addRepair);
 router.put("/", apiController.updateRepair);
+router.get("/user", apiController.getNewestRepairs);
 router.get("/:id", apiController.getRepairById);
 
 module.exports = router;
