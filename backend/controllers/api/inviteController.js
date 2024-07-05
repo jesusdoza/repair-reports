b6702bf84d44c3b997db1dcb066e4f03b4cd0f11
jesusdoza <jesus.doza@gmail.com>
2024-07-05 08:44:27 -0500

    add error handling to inviteController

--- backend/controllers/api/inviteController.js
+++ backend/controllers/api/inviteController.js
@@ -1,12 +1,29 @@
 const Invite = require("../../models/Invite");
 
 const getInvite = async (req, res) => {
-  const invite = Invite.findOne({});
+  let inviteCode, invitePhrase;
 
-  if (!invite) {
-    res.status(404).send();
+  ({ inviteCode, invitePhrase } = req.body);
+
+  try {
+    if (!inviteCode || !invitePhrase)
+      throw new Error("no invite code or phrase");
+
+    const invite = await Invite.findOne({ inviteCode, invitePhrase });
+
+    if (!invite) {
+      res.status(404).send();
+      return;
+    }
+
+    res.send({ invite });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send({
+      message: "failed to get invite",
+      invite: { inviteCode, invitePhrase },
+    });
   }
-  res.send({ invite });
 };
 
 module.exports = { getInvite };
