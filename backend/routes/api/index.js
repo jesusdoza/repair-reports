b029aca025b9f3108b010de78b0e46784294dea4
jesusdoza <jesus.doza@gmail.com>
2024-07-05 08:20:53 -0500

    add invite post and get routes

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,12 +4,16 @@ const { ensureAuthApi } = require("../../middleware/auth");
 const repairRouter = require("./repairs");
 const signatureRouter = require("./signature.js");
 const imagesRouter = require("./imagesRouter.js");
+const inviteController = require("../../controllers/api/inviteController.js");
 
 // /api/*
 
 router.post("/login", apiAuthController.apiLogin);
 router.get("/logout", apiAuthController.apiLogout);
 router.post("/signup", apiAuthController.apiSignup);
+
+router.get("/invite", inviteController.getInvite);
+router.post("/invite", inviteController.getInvite);
 
 router.use("/repairs", ensureAuthApi, repairRouter);
 router.use("/signform", ensureAuthApi, signatureRouter);
