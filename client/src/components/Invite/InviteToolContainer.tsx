c1c87f62593d052d8c2a390e88a5ba69a444287a
jesus doza <jesus.doza@gmail.com>
2024-07-09 10:20:41 -0500

    fix styling

--- client/src/components/Invite/InviteToolContainer.tsx
+++ client/src/components/Invite/InviteToolContainer.tsx
@@ -3,7 +3,7 @@ import InviteTool from "./InviteTool";
 export default function InviteToolContainer() {
   //todo get any invites user has pending
   return (
-    <div className="p-1">
+    <div className="p-1 bg-slate-600">
       <InviteTool />
     </div>
   );
--- client/src/pages/Profile/ProfilePage.tsx
+++ client/src/pages/Profile/ProfilePage.tsx
@@ -3,15 +3,18 @@ import UserInfoContainer from "../../components/Profile/UserInfoContainer";
 
 export default function ProfilePage(): React.ReactNode {
   return (
-    <section className="text-slate-400 flex h-[400px]">
-      <main className="w-4/6 bg-red-950 m-0 h-full p-1">
-        <UserInfoContainer />
+    <section className="text-slate-400 bg-emerald-400  flex flex-col ">
+      <div className="flex">
+        <main className="w-4/6 min-h-[400px] bg-red-950 m-0 p-1">
+          <UserInfoContainer />
+        </main>
 
-        <InviteToolContainer />
-      </main>
-
-      {/* side bar */}
-      <aside className="w-2/6  h-full m-0 bg-green-900 p-1"></aside>
+        {/* side bar */}
+        <aside className="w-2/6 min-h-[400px] m-0 bg-green-900 p-1">
+          aside
+        </aside>
+      </div>
+      <InviteToolContainer />
     </section>
   );
 }
