0cad7fb054587d8aba74304ac999d0e70e3fb7c5
jesus doza <jesus.doza@gmail.com>
2024-07-09 10:13:49 -0500

    create invite tools scaffolding

--- client/src/components/Invite/InviteTool.tsx
+++ client/src/components/Invite/InviteTool.tsx
@@ -1,3 +1,52 @@
-export default function InviteTool() {
-  return <div>InviteTool</div>;
+type InviteT = {
+  inviteCode: string;
+  invitePassword: string;
+  groupsId: string[];
+  createdAt: string;
+};
+
+type InviteToolPropsT = {
+  invites?: InviteT[];
+};
+
+const testInvites = [
+  {
+    inviteCode: "string",
+    invitePassword: "string",
+    groupsId: ["1234", "1234533"],
+    createdAt: "string",
+  },
+];
+
+export default function InviteTool({
+  invites = testInvites,
+}: InviteToolPropsT) {
+  return (
+    <table>
+      <tr>
+        <th>Invite Code</th>
+        <th>Invite Password</th>
+        <th>Groups</th>
+      </tr>
+      {invites.map((inv) => {
+        return <InviteListing invite={inv} />;
+      })}
+    </table>
+  );
+}
+
+function InviteListing({ invite }: { invite: InviteT }) {
+  return (
+    <tr>
+      <td>{invite.inviteCode}</td>
+      <td>{invite.invitePassword}</td>
+      <td>
+        <ul>
+          {invite.groupsId.map((id) => (
+            <li>{id}</li>
+          ))}
+        </ul>
+      </td>
+    </tr>
+  );
 }
--- client/src/components/Invite/InviteToolContainer.tsx
+++ client/src/components/Invite/InviteToolContainer.tsx
@@ -1,4 +1,10 @@
+import InviteTool from "./InviteTool";
+
 export default function InviteToolContainer() {
   //todo get any invites user has pending
-  return <div>InviteToolContainer</div>;
+  return (
+    <div className="p-1">
+      <InviteTool />
+    </div>
+  );
 }
