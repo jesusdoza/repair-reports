c8eca82447c6ea3bec3c3487b8f48fd19f8fb5b2
jesus doza <jesus.doza@gmail.com>
2024-07-09 09:54:22 -0500

    create profile page

---
+++ client/src/pages/Profile/ProfilePage.tsx
@@ -0,0 +1,14 @@
+import UserInfoContainer from "../../components/Profile/UserInfoContainer";
+
+export default function ProfilePage(): React.ReactNode {
+  return (
+    <section className="text-slate-400 flex h-[400px]">
+      <main className="w-4/6 bg-red-950  m-0 h-full p-1">
+        <UserInfoContainer />
+      </main>
+
+      {/* side bar */}
+      <aside className="w-2/6  h-full m-0 bg-green-900 p-1"></aside>
+    </section>
+  );
+}
