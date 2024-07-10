5d4c1d3a975211b018377c3c24f94880e8b807d7
jesus doza <jesus.doza@gmail.com>
2024-07-10 09:34:28 -0500

    add data invite data fetch to InviteToolContainer

--- client/src/components/Invite/InviteToolContainer.tsx
+++ client/src/components/Invite/InviteToolContainer.tsx
@@ -1,10 +1,29 @@
+import { useEffect, useState } from "react";
+import useInviteManager from "../../hooks/useInviteManager";
+import InviteLog from "./InviteLog";
 import InviteTool from "./InviteTool";
+
+type InviteT = {
+  inviteCode: string;
+  invitePassword: string;
+  groups: { id: string; name: string }[];
+  createdAt: string;
+  status: string;
+};
 
 export default function InviteToolContainer() {
   //todo get any invites user has pending
+  const { getUserInvites, data: inviteData } = useInviteManager();
+  // const [invites, setInvites] = useState<InviteT[]>([]);
+
+  useEffect(() => {
+    getUserInvites();
+  }, []);
+
   return (
     <div className="p-1 ">
       <InviteTool />
+      <InviteLog invites={inviteData} />
     </div>
   );
 }
