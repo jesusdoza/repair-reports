fbd09613a41438b068e446586b504bed543c0db0
jesus doza <jesus.doza@gmail.com>
2024-07-10 08:28:18 -0500

    add getUserInvites method to useInviteManager hook

--- client/src/api/InviteApi.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-const API_URL = import.meta.env.VITE_API_URL;
-
-function getUserInvites() {}
-function postInvite() {}
-
-export { getUserInvites, postInvite };
--- client/src/hooks/useInviteManager.ts
+++ client/src/hooks/useInviteManager.ts
@@ -1,3 +1,28 @@
+import axios, { AxiosError } from "axios";
+const API_URL = import.meta.env.VITE_API_URL;
+import useAuthContext from "./useAuthContext";
+
 export default function useInviteManager() {
-  return {};
+  const { unauthorizedError } = useAuthContext();
+
+  async function getUserInvites() {
+    try {
+      const response = await axios.get(`${API_URL}/api/invite`, {
+        withCredentials: true,
+      });
+
+      return response.data;
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        if (error?.response?.status && error?.response?.status == 401) {
+          console.log("unauthorized error @useRepairApi.getLatestRepairs");
+
+          unauthorizedError();
+        }
+      }
+    }
+  }
+  function postInvite() {}
+
+  return { getUserInvites, postInvite };
 }
