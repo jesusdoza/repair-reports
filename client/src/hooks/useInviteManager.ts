b52a6ae20bd7ce006a7852eb1eeddaded804fe3d
jesus doza <jesus.doza@gmail.com>
2024-07-10 09:38:40 -0500

    create type for invite data returned from server

--- client/src/hooks/useInviteManager.ts
+++ client/src/hooks/useInviteManager.ts
@@ -3,9 +3,17 @@ const API_URL = import.meta.env.VITE_API_URL;
 import useAuthContext from "./useAuthContext";
 import { useState } from "react";
 
+type InviteDataT = {
+  inviteCode: string;
+  invitePassword: string;
+  groups: { id: string; name: string }[];
+  createdAt: string;
+  status: string;
+};
+
 export default function useInviteManager() {
   const { unauthorizedError } = useAuthContext();
-  const [data, setData] = useState([]);
+  const [data, setData] = useState<InviteDataT[]>([]);
 
   async function getUserInvites() {
     try {
@@ -13,7 +21,6 @@ export default function useInviteManager() {
         withCredentials: true,
       });
 
-      console.log("response.data", response.data.invites);
       setData(response.data.invites);
     } catch (error) {
       if (error instanceof AxiosError) {
