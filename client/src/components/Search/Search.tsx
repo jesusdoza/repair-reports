3f8bbe7c6472dca9a599fed690c87dd4d505f70c
jesus doza <jesus.doza@gmail.com>
2024-07-15 09:32:50 -0500

    change search widget now navigates to searchpage

--- client/src/components/Search/Search.tsx
+++ client/src/components/Search/Search.tsx
@@ -1,17 +1,31 @@
 import React, { useState } from "react";
 import useRepairApi from "../../hooks/useRepairApi";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export default function Search(): React.ReactNode {
   const { searchForRepair } = useRepairApi();
+  const location = useLocation();
+  const navigate = useNavigate();
 
   const [search, setSearch] = useState("");
   const handleSearch = async (event: React.SyntheticEvent) => {
     event.preventDefault();
-    console.log("search", search);
-    if (search) {
-      const results = await searchForRepair(search);
-      console.log("results", results);
+
+    const searchParams = new URLSearchParams();
+    searchParams.append("search", search);
+
+    const navigateTo = `/search?${searchParams.toString()}`;
+    const currentLoc = `${location.pathname}${location.search}`;
+
+    //only navigate to search page if current page is not the same
+    if (navigateTo == currentLoc) {
+      return;
     }
+
+    console.log("location.pathname", location.pathname);
+    console.log("location.search", location.search);
+
+    navigate(navigateTo);
   };
   return (
     <form onSubmit={handleSearch}>
