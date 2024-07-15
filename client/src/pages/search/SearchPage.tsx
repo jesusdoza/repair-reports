76be31a423f1a76fff96dfea1a43e2f4d7d77795
jesus doza <jesus.doza@gmail.com>
2024-07-15 09:34:42 -0500

    add SearchPage component feature

--- client/src/pages/search/SearchPage.tsx
+++ client/src/pages/search/SearchPage.tsx
@@ -1,25 +1,46 @@
-import { useParams, useSearchParams } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 import RepairList from "../../components/RepairList/RepairList";
-
-// type SearchPagePropsT = {
-//   search?: string;
-//   limit?: number;
-//   page?: number;
-// };
+import useRepairApi from "../../hooks/useRepairApi";
+import { useEffect, useState } from "react";
+import { RepairDataT } from "../../../types";
 
 export default function SearchPage() {
-  // const { search, limit, page } = useParams();
+  const { searchForRepair } = useRepairApi();
   const [searchParams] = useSearchParams();
+  const [repairsFound, setRepairsFound] = useState<RepairDataT[]>([]);
 
-  const limit = searchParams.get("limit");
+  //todo add limit and page number to request
+  // const limit = searchParams.get("limit") || 10;
+  // const page = searchParams.get("page") || 1;
   const search = searchParams.get("search");
-  const page = searchParams.get("page");
 
-  console.log("search, limit, page,", search, limit, page);
+  async function getSearchResults(search: string) {
+    const results = await searchForRepair(search);
+
+    console.log("results", results);
+    if (results) {
+      setRepairsFound(results);
+    }
+  }
+
+  useEffect(() => {
+    if (search) {
+      getSearchResults(search);
+    }
+  }, [searchParams]);
+
+  if (!search) {
+    return (
+      <div>
+        <p>No search phrase provided</p>
+      </div>
+    );
+  }
+
   return (
     <div>
       <p>Search Results for {search}</p>
-      <RepairList repairList={[]} />
+      <RepairList repairList={repairsFound} />
     </div>
   );
 }
