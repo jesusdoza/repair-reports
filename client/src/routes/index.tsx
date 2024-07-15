5880334d16bf93cd0930360d0a83fcee8aa7e7b3
jesusdoza <jesus.doza@gmail.com>
2024-07-14 20:33:40 -0500

    add search route to react

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -6,6 +6,7 @@ import RepairFormPage from "../pages/CreateRepairFormPage";
 import DashboardPage from "../pages/dashboard/DashboardPageContainer";
 import { RepairInfoPageContainer } from "../pages/RepairInfoPageContainer";
 import EditRepairPageV2 from "../pages/EditRepairPageV2";
+import SearchPage from "../pages/search/SearchPage";
 
 export const routes = createBrowserRouter([
   {
@@ -64,6 +65,14 @@ export const routes = createBrowserRouter([
     element: (
       <Layout>
         <LatestRepairsPage />
+      </Layout>
+    ),
+  },
+  {
+    path: "/search",
+    element: (
+      <Layout>
+        <SearchPage />
       </Layout>
     ),
   },
