033416ae74f00740f0a45e0acf1830d326f8a993
jesus doza <jesus.doza@gmail.com>
2024-07-23 09:56:38 -0500

    add test for render without props, test with image url

--- client/tests/components/EditImageCard.test.tsx
+++ client/tests/components/EditImageCard.test.tsx
@@ -1,14 +1,36 @@
-import { EditImageCard } from "../../src/components/ImageCard/EditImageCard";
-import { render, screen, fireEvent } from "@testing-library/react";
+import EditImageCardContainer from "../../src/components/ImageCard/EditImageCard";
+import { render, screen, fireEvent, within, act } from "@testing-library/react";
 import "@testing-library/jest-dom/vitest";
 
 describe("EditImageCard tests", () => {
+  const testImageUrl = "https://placekitten.com/200/300";
+
   it("render with no props", () => {
-    render(<EditImageCard />);
-    // screen.get
+    render(<EditImageCardContainer />);
+    screen.getByTestId("edit-image-card");
   });
 
-  it("should display image url", () => {});
+  it("should display image from url", () => {
+    const { container } = render(<EditImageCardContainer url={testImageUrl} />);
+
+    const imgElement = container.querySelector("img");
+
+    expect(imgElement?.src).toBe(testImageUrl);
+  });
+
+  // it("should display message on eror", () => {
+  //   const { container } = render(
+  //     <EditImageCardContainer onRemove={() => console.log("test error")} />
+  //   );
+
+  //   const deleteBtn = screen.getByTestId("delete-button");
+
+  //   act(() => {
+  //     fireEvent.click(deleteBtn);
+  //   });
+
+  //   screen.debug();
+  // });
 
   it("should display camera when activated", () => {});
 
