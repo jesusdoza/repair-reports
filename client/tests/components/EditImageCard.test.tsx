a3427a8d75cf01e539e45d97bec17275d3698818
jesusdoza <jesus.doza@gmail.com>
2024-07-23 08:31:10 -0500

    test scaffold

--- client/tests/components/EditImageCard.test.tsx
+++ client/tests/components/EditImageCard.test.tsx
@@ -1,9 +1,16 @@
 import { EditImageCard } from "../../src/components/ImageCard/EditImageCard";
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom/vitest";
 
 describe("EditImageCard tests", () => {
-  it("render with no props", () => {});
+  it("render with no props", () => {
+    render(<EditImageCard />);
+    // screen.get
+  });
 
   it("should display image url", () => {});
 
   it("should display camera when activated", () => {});
+
+  it("should display uploaded status on http urls", () => {});
 });
