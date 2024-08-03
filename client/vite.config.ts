a3f6629bdc6e749d59bef84421c30076575983fe
jesus doza <jesus.doza@gmail.com>
2024-08-03 16:28:13 -0500

    test config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV == "production" ? "/react" : "/",
-  // build: { outDir: "../backend/public/react", emptyOutDir: true },
+  base: process.env.VITE_BASE,
 });
