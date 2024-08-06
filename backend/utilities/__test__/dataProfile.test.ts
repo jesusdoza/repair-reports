e2546ed01ad7acaf04af31899fc9b7d8e4c7bef3
jesus doza <jesus.doza@gmail.com>
2024-08-06 09:36:22 -0500

    add dataProfile set up

---
+++ backend/utilities/__test__/dataProfile.test.ts
@@ -0,0 +1,8 @@
+import { it, expect, describe, test } from "vitest";
+import dataProfile from "../dataProfile";
+
+describe("dataProfile", () => {
+  it("accept path string to json and return object with ", () => {
+    const result = dataProfile("./");
+  });
+});
