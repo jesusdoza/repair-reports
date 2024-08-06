fd84afb3f128f7447fb39bfcb90686e2f2542e6f
jesus doza <jesus.doza@gmail.com>
2024-08-06 10:31:37 -0500

    set up test scaffold

--- backend/utilities/__test__/dataProfile.test.ts
+++ backend/utilities/__test__/dataProfile.test.ts
@@ -1,8 +1,24 @@
 import { it, expect, describe, test } from "vitest";
-import dataProfile from "../dataProfile";
+import dataProfile from "../dataProfile.ts";
+
+// import dataJson from "../../ignoreFiles/repairs_8_6_24.json";
+
+// const testData = dataJson;
 
 describe("dataProfile", () => {
-  it("accept path string to json and return object with ", () => {
-    const result = dataProfile("./");
+  // it("accept path string to json and return object with ", async () => {
+  //   const result = await dataProfile("../../ignoreFiles/repairs_8_6_24.json");
+
+  //   expect(typeof result).toBe("object");
+  // });
+
+  it("should error gracefully if path to json is invalid", async () => {
+    const result = await dataProfile("./no file");
+
+    console.log("result.error", result.error);
+    expect(result.error).toBeTruthy();
   });
+  it.todo("should return amount of objects parsed", () => {});
+  it.todo("should return array of different object patterns found", () => {});
+  it.todo("should return ", () => {});
 });
--- backend/utilities/dataProfile.ts
+++ backend/utilities/dataProfile.ts
@@ -1,4 +1,22 @@
+import { promises as fs } from "fs";
 
-export default dataProfile(){
-    
+export default async function dataProfile(path: string) {
+  let stream;
+
+  try {
+    await fs.open("myfile", "r");
+  } catch (error) {
+    return { error };
+  }
+
+  //   try {
+  //     stream = await fs.createReadStream(path, {
+  //       encoding: "utf-8",
+  //     });
+
+  //     return {};
+  //   } catch (error) {
+  //     console.log("stream failed");
+  //     return { error: "failed to load file" };
+  //   }
 }
