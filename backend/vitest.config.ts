d80482b0ca229371aabc577a8590a7a25e8d5437
jesus doza <jesus.doza@gmail.com>
2024-08-06 10:31:30 -0500

    set up vitest on backend

---
+++ backend/vitest.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig, mergeConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    include: ["**/*.test.tsx", "**/*.test.ts"],
+    globals: true,
+    // environment: "jsdom",
+    //   setupFiles: "tests/setup.ts",
+    coverage: {
+      reporter: ["text", "json", "html"],
+      provider: "v8",
+    },
+  },
+});
