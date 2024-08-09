61c72b45aa4ef8266d94df164f7ce16133e9a32b
jesus doza <jesus.doza@gmail.com>
2024-08-09 10:16:13 -0500

    add test dir scaffold

---
+++ client/tests/components/RepairDisplay/EditRepairForm.test.tsx
@@ -0,0 +1,12 @@
+import { act, fireEvent, render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/vitest";
+import { it, expect, describe } from "vitest";
+import React from "react";
+
+import EditRepairForm from "../../../src/components/RepairDisplay/RepairEditFormV2";
+
+describe("EditRepairForm", () => {
+  it("display repair data", () => {});
+  it("call edit function passed in", () => {});
+  it("should handle invalid repair data", () => {});
+});
