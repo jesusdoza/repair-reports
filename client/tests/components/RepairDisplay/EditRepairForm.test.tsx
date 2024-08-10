45180424720d42329dcdc638c14ede61a3956f77
jesus doza <jesus.doza@gmail.com>
2024-08-09 19:29:39 -0500

    add display data test client/src/components/RepairDisplay/RepairEditFormV2.tsx

--- client/tests/components/RepairDisplay/EditRepairForm.test.tsx
+++ client/tests/components/RepairDisplay/EditRepairForm.test.tsx
@@ -4,9 +4,41 @@ import { it, expect, describe } from "vitest";
 import React from "react";
 
 import EditRepairForm from "../../../src/components/RepairDisplay/RepairEditFormV2";
+import { Repair } from "../../../src/classes/Repair";
+import { RepairDataT } from "../../../types";
+
+const testData: RepairDataT = {
+  boardType: "test board",
+  createdBy: "test id",
+  engineMake: "test enginemake",
+  group: "test group",
+  removed: false,
+  procedureArr: [],
+  title: "test title",
+  version: 3,
+  searchTags: ["test tag"],
+  _id: "111111",
+  visibility: "public",
+};
+
+const testRepair = new Repair(testData);
 
 describe("EditRepairForm", () => {
-  it("display repair data", () => {});
-  it("call edit function passed in", () => {});
-  it("should handle invalid repair data", () => {});
+  it("display error with missing data", () => {
+    //@ts-expect-error checking empty prop render
+    render(<EditRepairForm />);
+    screen.getByText(/no data/i);
+  });
+  it("display data", () => {
+    //@ts-expect-error checking empty prop render
+    render(<EditRepairForm repair={testRepair} />);
+    screen.debug();
+    screen.getByText(/test title/i);
+  });
+  it.todo("call edit function passed in", () => {});
+  it.todo(
+    "repair form changes should be reflected on call function passed in",
+    () => {}
+  );
+  it.todo("should handle invalid repair data", () => {});
 });
