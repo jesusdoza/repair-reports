a5eb606d08f16bb488b00d5e8ef4acfc7a3fce04
jesus doza <jesus.doza@gmail.com>
2024-08-13 20:07:33 -0500

    add mock env file for tests

---
+++ backend/tests/mockEnv.ts
@@ -0,0 +1,17 @@
+import { vi } from "vitest";
+
+vi.stubEnv("NODE_ENV", "development");
+// cloud_name = dafdsfad
+// cloud_key = 12412412
+// cloud_secret = 412341234213fdA7dfa
+// cloud_folder = test
+
+// PORT = 8000
+// NODE_ENV = development
+
+// session_secret = "rqwerqwr2332341u"
+
+// search_index=""
+// connect_string = mongodb+srv://USERNAME:PASSWORD@cluster0.losdw.mongodb.net/?retryWrites=true&w=majority
+
+// client_origin= "http://localhost:5173"
