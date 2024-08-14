193c2fd9e1c7f2ef82f151cbe9e59a8911aec16a
jesus doza <jesus.doza@gmail.com>
2024-08-13 22:08:47 -0500

    clean up

--- backend/tests/testEnvSetup/mockEnv.ts
+++ backend/tests/testEnvSetup/mockEnv.ts
@@ -16,13 +16,7 @@ vi.stubEnv("connect_string", uriEnv);
 // cloud_key = 12412412
 // cloud_secret = 412341234213fdA7dfa
 // cloud_folder = test
-
 // PORT = 8000
 // NODE_ENV = development
-
-// session_secret = "rqwerqwr2332341u"
-
 // search_index=""
-// connect_string = mongodb+srv://USERNAME:PASSWORD@cluster0.losdw.mongodb.net/?retryWrites=true&w=majority
-
 // client_origin= "http://localhost:5173"
