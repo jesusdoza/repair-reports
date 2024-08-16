d564fbfec2edcfd856ef00f7dc53bbe939a17c6c
jesusdoza <jesus.doza@gmail.com>
2024-08-16 08:44:53 -0500

    add modal component

---
+++ client/src/components/Modal/Modal.tsx
@@ -0,0 +1,27 @@
+import { ReactNode } from "react";
+type ModalProps = {
+  isOpen: boolean;
+  onClose: () => void;
+  children: ReactNode;
+};
+
+export default function Modal({ children, isOpen, onClose }: ModalProps) {
+  if (!isOpen) return null;
+
+  return (
+    <div
+      className="modal-overlay"
+      onClick={onClose}>
+      <div
+        className="modal-content"
+        onClick={(e) => e.stopPropagation()}>
+        <button
+          className="modal-close"
+          onClick={onClose}>
+          &times;
+        </button>
+        {children}
+      </div>
+    </div>
+  );
+}
