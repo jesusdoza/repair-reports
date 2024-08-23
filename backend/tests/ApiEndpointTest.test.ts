2b8776e08eb9c00d54f9151d3ee90a19a84e0eba
jesusdoza <jesus.doza@gmail.com>
2024-08-22 21:21:01 -0500

    add groupController tests

--- backend/tests/ApiEndpointTest.test.ts
+++ backend/tests/ApiEndpointTest.test.ts
@@ -2,7 +2,6 @@ import { it, expect, describe, test, vi, afterAll, beforeAll } from "vitest";
 import request from "supertest";
 import { App } from "supertest/types.js";
 
-// import { setupDatabase, teardownDatabase } from "./testEnvSetup/setupTests.js";
 import "./testEnvSetup/mockEnv.js"; //mock env variables loaded need to load before app wont work in same file for some reason
 
 import app from "../app.js";
--- backend/tests/groupControllerTests.test.ts
+++ backend/tests/groupControllerTests.test.ts
@@ -1,4 +1,13 @@
-import { it, expect, describe, test, vi, beforeAll, afterAll } from "vitest";
+import {
+  it,
+  expect,
+  describe,
+  test,
+  vi,
+  beforeAll,
+  afterAll,
+  beforeEach,
+} from "vitest";
 import "./testEnvSetup/mockEnv.js"; //mock env variables loaded need to load before app wont work in same file for some reason
 import mongoose from "mongoose";
 import request from "supertest";
@@ -16,17 +25,26 @@ import {
   addMemberTogroup,
 } from "../controllers/api/groupsController.js";
 
-vi.mock("../models/Invite.js", () => ({
-  find: vi.fn().mockResolvedValue([{}, {}, {}]),
-}));
+const mockMemberEntries = [
+  { memberEntry: "fasdf" },
+  { memberEntry: "fasdf" },
+  { memberEntry: "fasdf" },
+  { memberEntry: "fasdf" },
+];
+const mockInviteEntries = [
+  { InviteEntry: "fasdf" },
+  { InviteEntry: "fasdf" },
+  { InviteEntry: "fasdf" },
+];
 
-vi.mock("'../models/Member.js'", () => ({
-  find: vi.fn().mockResolvedValue([{}, {}]),
-}));
+//MOCK MODELS FOR MONGOOSE
+mongoose.models.Member.find = vi.fn().mockResolvedValue(mockMemberEntries);
+mongoose.models.Invite.find = vi.fn().mockResolvedValue(mockInviteEntries);
 
+//
 const app = express();
 app.use(express.json());
-app.get("/api/groups", getUsersGroups);
+
 app.post("/api/groups/add", addMemberTogroup);
 
 beforeAll(async () => {
@@ -37,13 +55,43 @@ afterAll(async () => {
   await teardownDatabase();
 });
 
-describe("group", () => {
+const requestIntercept = vi.fn().mockImplementation((req) => {
+  req.user = { _id: "123" };
+  return req;
+});
+
+app.get("/api/groups", (req: Request, res: Response) => {
+  // req.user = { _id: "125" };
+  req = requestIntercept(req);
+  getUsersGroups(req, res);
+});
+
+describe("groupController test", () => {
   const application = app as unknown as App;
 
-  it("should return 401 on missing user or id missing", async () => {
-    const response = await request(application)
-      .get("/api/groups")
-      .set("user", JSON.stringify({}));
+  it("should return 200 when user object is present", async () => {
+    //setup as if user is authenticated
+
+    const response = await request(application).get("/api/groups");
+    expect(response.statusCode).toBe(200);
+  });
+
+  it("should return array of objects", async () => {
+    const response = await request(application).get("/api/groups");
+
+    console.log("response", response.body);
+
+    expect(response.statusCode).toBe(200);
+    expect(Array.isArray(response.body)).toBe(true);
+    expect(response.body.length).toBe(mockMemberEntries.length);
+  });
+
+  it("should return 401 when no user id is found", async () => {
+    requestIntercept.mockImplementationOnce((req) => {
+      return req;
+    });
+
+    const response = await request(application).get("/api/groups");
 
     expect(response.statusCode).toBe(401);
   });
