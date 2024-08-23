562a571e77ac41f52f2cfb0c22b489a2059646c1
jesusdoza <jesus.doza@gmail.com>
2024-08-22 21:46:51 -0500

    add addMemberToGroup() to controller

--- backend/controllers/api/groupsController.js
+++ backend/controllers/api/groupsController.js
@@ -19,6 +19,8 @@ const addMemberTogroup = async (req, res) => {
   const invitecode = req.body.inviteCode;
   const password = req.body?.password;
 
+  const user = req.user;
+
   //find invite
   const foundInvite = await Invite.find({ inviteCode: invitecode });
 
@@ -26,12 +28,39 @@ const addMemberTogroup = async (req, res) => {
 
   if (!foundInvite) {
     res.status(401).send();
+    return;
   }
 
+  if (foundInvite.password && foundInvite.password == password) {
+    res.status(401).send();
+    return;
+  }
+
+  const promises = createGroupMemberEntries(foundInvite.groups, user);
+
+  await Promise.allSettled(promises);
+
   //return status 201
-
-  //return 401 on error or bad invite
-
-  res.send({ post: "members" });
+  res.status(201).send();
 };
 module.exports = { getUsersGroups, addMemberTogroup };
+
+//create member of group entries promises
+/**
+ *
+ * @param {groups, user} groups :{id,name}[], user:{_id, username}
+ * @returns
+ */
+function createGroupMemberEntries(groups = [], user) {
+  console.log("groups", groups);
+  return groups.map((group) => {
+    const entry = new Member({
+      groupId: group.id,
+      groupName: group.name,
+      role: ["read"],
+      userId: user._id,
+      username: user.username,
+    });
+    return entry.save();
+  });
+}
