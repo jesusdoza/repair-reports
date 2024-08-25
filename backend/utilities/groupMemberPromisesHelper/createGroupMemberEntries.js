4341495a954dafb60e116fe35f7d27c71e33e265
jesus doza <jesus.doza@gmail.com>
2024-08-25 11:15:48 -0500

    add  invite update existing membership

--- backend/utilities/groupMemberPromisesHelper/createGroupMemberEntries.js
+++ backend/utilities/groupMemberPromisesHelper/createGroupMemberEntries.js
@@ -5,20 +5,50 @@ const Member = require("../../models/Member");
 
 /**
  *
- * @param {groups, user} groups :{id,name}[], user:{_id, username}
+ * @param {groups, user} groups :{id,name,roles:String[]}[], user:{_id, username}
  * @returns
  */
 function createGroupMemberEntries(groups = [], user) {
-  console.log("groups", groups);
-  return groups.map((group) => {
-    const entry = new Member({
-      groupId: group.id,
-      groupName: group.name,
-      role: ["read"],
-      userId: user._id,
-      username: user.username,
-    });
-    return entry.save();
+  return groups.map(async (group) => {
+    //check if already have group entry
+    let foundMemberShip;
+    const newRoles = group.roles;
+
+    try {
+      foundMemberShip = await Member.findOne({
+        userId: user._id,
+        groupId: group.id,
+      });
+    } catch (error) {
+      console.log("error finding memberships ");
+    }
+
+    //existing membership update
+
+    try {
+      if (foundMemberShip) {
+        const updatedRoles = Array.from(
+          new Set([...foundMemberShip.roles, ...newRoles]).values()
+        );
+
+        foundMemberShip.roles = updatedRoles;
+
+        return foundMemberShip.save();
+      } else {
+        const entry = new Member({
+          groupId: group.id,
+          groupName: group.name,
+          role: newRoles,
+          userId: user._id,
+          username: user.username,
+        });
+
+        console.log("entry", entry);
+        return entry.save();
+      }
+    } catch (error) {
+      console.log("error saving membership", error);
+    }
   });
 }
 
