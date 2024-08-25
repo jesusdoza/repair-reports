72f6434463cf425f87f3379b703fe026162372c7
jesus doza <jesus.doza@gmail.com>
2024-08-25 12:59:04 -0500

    add tests for membersController

--- backend/tests/memberControllerTests.test.ts
+++ backend/tests/memberControllerTests.test.ts
@@ -9,10 +9,13 @@ import {
   beforeEach,
 } from "vitest";
 import "./testEnvSetup/mockEnv.js"; //mock env variables loaded need to load before app wont work in same file for some reason
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 import request from "supertest";
 import express from "express";
 import { App } from "supertest/types.js";
+
+import Member from "../models/Member.js";
+import Invite from "../models/Invite.js";
 
 import "./testEnvSetup/mockEnv.js"; //mock env variables loaded need to load before app wont work in same file for some reason
 
@@ -25,27 +28,36 @@ import {
   addMemberTogroup,
 } from "../controllers/api/membersController.js";
 
-const mockMemberEntries = [
-  { memberEntry: "fasdf" },
-  { memberEntry: "fasdf" },
-  { memberEntry: "fasdf" },
-  { memberEntry: "fasdf" },
-];
-const mockInviteEntries = [
-  { InviteEntry: "fasdf" },
-  { InviteEntry: "fasdf" },
-  { InviteEntry: "fasdf" },
-];
+const mockMemberEntries = [new Member({ userId: "userId", groupId: "group1" })];
+const mockInviteEntries = new Invite({
+  inviteCode: "1111",
+  password: "password",
+  groups: [
+    {
+      roles: ["testing group1 role"],
+      id: "group1",
+      name: "group 1 name",
+    },
+    {
+      roles: ["testing group 1 role"],
+      id: "group2",
+      name: "group 2 name",
+    },
+  ],
+});
+
+const requestIntercept = vi.fn().mockImplementation((req) => {
+  req.user = { _id: "userId" };
+  return req;
+});
 
 //MOCK MODELS FOR MONGOOSE
-mongoose.models.Member.find = vi.fn().mockResolvedValue(mockMemberEntries);
-mongoose.models.Invite.find = vi.fn().mockResolvedValue(mockInviteEntries);
+Member.find = vi.fn().mockResolvedValue(mockMemberEntries);
+Invite.findOne = vi.fn().mockResolvedValue(mockInviteEntries);
 
-//
+//test app instance
 const app = express();
 app.use(express.json());
-
-app.post("/api/groups/add", addMemberTogroup);
 
 beforeAll(async () => {
   await setupDatabase();
@@ -55,12 +67,14 @@ afterAll(async () => {
   await teardownDatabase();
 });
 
-const requestIntercept = vi.fn().mockImplementation((req) => {
-  req.user = { _id: "123" };
-  return req;
+//TEST ROUTES
+const getMemberships = "/members/user";
+const postMembership = "/members/join";
+app.post(postMembership, (req: Request, res: Response) => {
+  req = requestIntercept(req);
+  addMemberTogroup(req, res);
 });
-
-app.get("/api/groups", (req: Request, res: Response) => {
+app.get(getMemberships, (req: Request, res: Response) => {
   // req.user = { _id: "125" };
   req = requestIntercept(req);
   getUsersGroups(req, res);
@@ -69,26 +83,54 @@ app.get("/api/groups", (req: Request, res: Response) => {
 describe("groupController test", () => {
   const application = app as unknown as App;
 
-  it("should return 200 when user object is present", async () => {
-    const response = await request(application).get("/api/groups");
+  it("should return 200 when user object is present and list of entries getMemberships", async () => {
+    const response = await request(application).get(getMemberships);
     expect(response.statusCode).toBe(200);
-  });
 
-  it("should return array of objects", async () => {
-    const response = await request(application).get("/api/groups");
-
-    expect(response.statusCode).toBe(200);
-    expect(Array.isArray(response.body)).toBe(true);
     expect(response.body.length).toBe(mockMemberEntries.length);
   });
 
-  it("should return 401 when user is not authenticated is found", async () => {
+  it("should return 401 when user is not authenticated getMemberships", async () => {
     //remove user authentication to mock invalid user
     requestIntercept.mockImplementationOnce((req) => {
       return req;
     });
 
-    const response = await request(application).get("/api/groups");
+    const response = await request(application).get(getMemberships);
+
+    expect(response.statusCode).toBe(401);
+  });
+
+  it("should return 401 when user is not authenticated members/join POST", async () => {
+    //remove user authentication to mock invalid user
+    requestIntercept.mockImplementationOnce((req) => {
+      return req;
+    });
+
+    const response = await request(application).post(postMembership);
+
+    expect(response.statusCode).toBe(401);
+  });
+
+  it("should return 201 when user sends valid invite code and password to have group membership created POST", async () => {
+    //remove user authentication to mock invalid user
+    const response = await request(application)
+      .post(postMembership)
+      .send({ inviteCode: "1111", password: "password" });
+
+    expect(response.statusCode).toBe(201);
+  });
+  it("should return 401 when user sends missing invite code or invalid password to have group membership created POST", async () => {
+    //remove user authentication to mock invalid user
+    let response = await request(application)
+      .post(postMembership)
+      .send({ inviteCode: "1111", password: "wrong password" });
+
+    expect(response.statusCode).toBe(401);
+
+    response = await request(application)
+      .post(postMembership)
+      .send({ inviteCode: undefined, password: "password" });
 
     expect(response.statusCode).toBe(401);
   });
