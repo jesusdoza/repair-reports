6ff1bfbf4507427d324a6a2e254332f7e5706a30
jesus doza <jesus.doza@gmail.com>
2024-09-02 17:26:33 -0500

    add image to target procedure test

--- client/tests/context/RepairFormContext.test.ts
+++ client/tests/context/RepairFormContext.test.ts
@@ -18,6 +18,7 @@ import { Repair } from "../../src/classes/Repair";
 
 import { useContext } from "react";
 import { Procedure } from "../../src/classes/Procedure";
+import { ImageObj } from "../../src/classes/ImageObj";
 
 describe("repair form context method tests", () => {
   it("should change title on repair data changeTitle()", () => {
@@ -32,7 +33,7 @@ describe("repair form context method tests", () => {
   });
 });
 
-describe("RepairFormContext tests", () => {
+describe("RepairFormContext form changes tests", () => {
   it("should initialize repairFormdata with initializeRepairformData method", async () => {
     const { result } = renderHook(() => useContext(RepairFormDataContext), {
       wrapper: RepairContextProvider,
@@ -112,12 +113,6 @@ describe("RepairFormContext tests", () => {
       result.current.repairFormData.procedureArr[addAfterPos + 2]._id
     ).toBe(pushedProcId);
 
-    //SECOND TEST
-    // console.log(
-    //   "before : ",
-    //   result.current.repairFormData.procedureArr.map((i) => i._id)
-    // );
-
     addAfterPos = 1;
     testProc = new Procedure();
     testProc._id += "new 2";
@@ -133,7 +128,6 @@ describe("RepairFormContext tests", () => {
       result.current.formAction.addProcedureAfter(targetId, testProc);
     });
 
-    //TODO FAILING TEST
     // expect new procedure to be inserted after the pos specified
     expect(
       result.current.repairFormData.procedureArr[addAfterPos + 1]._id
@@ -144,4 +138,90 @@ describe("RepairFormContext tests", () => {
       result.current.repairFormData.procedureArr[addAfterPos + 2]._id
     ).toBe(pushedProcId);
   });
+});
+
+describe("repairform context procedure change tests", () => {
+  it("changes target procedure instructions", async () => {
+    const newIntructions = "instructions changed to this";
+
+    const procedures = [new Procedure(), new Procedure(), new Procedure()];
+    const targetProcedureId = procedures[1]._id;
+    const initialRepairData = new Repair();
+    initialRepairData.procedureArr = procedures;
+    const { result } = renderHook(() => useContext(RepairFormDataContext), {
+      wrapper: RepairContextProvider,
+    });
+
+    await act(async () => {
+      await result.current.initializeRepairFormData(initialRepairData);
+    });
+    await act(async () => {
+      await result.current.formAction.updateInstructions(
+        targetProcedureId,
+        newIntructions
+      );
+    });
+
+    await waitFor(() => {
+      const procedures = result.current.repairFormData.procedureArr;
+
+      for (let i = 0; i < procedures.length; i++) {
+        const procedureObj = procedures[i];
+
+        if (procedureObj._id === targetProcedureId) {
+          expect(procedureObj.instructions).toEqual(newIntructions);
+        } else {
+          expect(procedureObj.instructions).toEqual("");
+        }
+      }
+    });
+  });
+
+  it("adds image to target procedure", async () => {
+    const newImageUrl = "http://new_url";
+
+    const procedures = [new Procedure(), new Procedure(), new Procedure()];
+    const targetProcedureId = procedures[0]._id;
+    const initialRepairData = new Repair();
+    initialRepairData.procedureArr = procedures;
+
+    const { result } = renderHook(() => useContext(RepairFormDataContext), {
+      wrapper: RepairContextProvider,
+    });
+
+    await act(async () => {
+      await result.current.initializeRepairFormData(initialRepairData);
+    });
+
+    await act(async () => {
+      const newImagebj = new ImageObj();
+      newImagebj.imageUrl = newImageUrl;
+      await result.current.formAction.addImage(newImagebj, targetProcedureId);
+    });
+
+    await waitFor(() => {
+      const procedures = result.current.repairFormData.procedureArr;
+
+      for (let i = 0; i < procedures.length; i++) {
+        const procedureObj = procedures[i];
+
+        procedureObj.imageObjs.forEach((data) => {
+          if (data.imageUrl === newImageUrl) {
+            expect(procedureObj._id).toBe(targetProcedureId);
+          } else {
+            expect(procedureObj._id).not.toBe(targetProcedureId);
+          }
+        });
+        //go through the images in all procedures to see if image url was addeded
+      }
+    });
+  });
+
+  it.todo("removes image from target procedure", () => {
+    expect(images).not.toContain(testImage);
+  });
+});
+
+describe("repairform context image change tests", () => {
+  it.todo("should", () => {});
 });
