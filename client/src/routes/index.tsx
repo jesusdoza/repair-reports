3fb75c0554b37b9d4f2c421a82d835f98b8e7ae5
jesus doza <jesus.doza@gmail.com>
2024-09-09 08:45:55 -0500

    dir-refactor

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -3,17 +3,17 @@ import { Layout } from "../Layout";
 import LatestRepairsPage from "../pages/LatestRepairsPage";
 import ProfilePage from "../pages/Profile/ProfilePage";
 import RepairFormPage from "../pages/CreateRepairFormPage";
-import DashboardPage from "../pages/dashboard/DashboardPageContainer";
+import DashboardPage from "../pages/Dashboard/DashboardPageContainer";
 import { RepairInfoPage } from "../pages/RepairInfoPage";
 import EditRepairPageV2 from "../pages/EditRepairPageV2";
-import SearchPage from "../pages/search/SearchPage";
+import SearchPage from "../pages/Search/SearchPage";
 import LogoutPage from "../pages/LogoutPage";
 
 import HomePage from "../pages/Home/HomePage";
 import ErrorBoundary from "../components/ErrorBoundary/ErrorBoundary";
 import LoginSignupContainer from "../components/LoginSignUp/LoginSignupContainer";
 import ProtectedRoute from "../components/ProtectedRoute";
-import InvitePage from "../pages/invite/InvitePage";
+import InvitePage from "../pages/Invite/InvitePage";
 
 export const routes = createBrowserRouter([
   {
