8973e676c03ff3cb73ba2e7812ab31d5d7da451a
jesus doza <jesus.doza@gmail.com>
2024-09-11 20:23:52 -0500

    add clerk signout to logout page

--- client/src/pages/LogoutPage.tsx
+++ client/src/pages/LogoutPage.tsx
@@ -1,8 +1,12 @@
 import useAuthContext from "../hooks/useAuthContext";
+import { useAuth } from "@clerk/clerk-react";
 
 export default function LogoutPage() {
   const { logout } = useAuthContext();
+  const { signOut } = useAuth();
 
+  signOut();
   logout && logout();
+
   return <div>LogoutPage</div>;
 }
