7767a38ba33ac65bce1ebf799bdb144471ec8d53
jesus doza <jesus.doza@gmail.com>
2024-09-14 11:10:29 -0500

    add signupSetupPage to deal with 3rd party auth profile set up

---
+++ client/src/pages/signup/SignupSetupPage.tsx
@@ -0,0 +1,54 @@
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import { useAuth, useUser } from "@clerk/clerk-react";
+import { useNavigate, useParams } from "react-router-dom";
+
+export default function SignupSetupPage() {
+  const { signUpWithProvider, isAuth, userInfo } = useContext(AuthContext);
+  // const { getInvite } = useInviteManager();
+  const { userId, ...clerkAuth } = useAuth();
+  const { user } = useUser();
+  const navigate = useNavigate();
+  const [errors, setErrors] = useState<string[]>([]);
+
+  // const password = location.hash.substring(1);
+  // const clerkAuthProvider = user?.externalAccounts[0].provider;
+
+  // console.log("password", password);
+  console.log("clerkAuth", clerkAuth);
+  // console.log("clerk provider", user?.externalAccounts[0].provider);
+
+  const email = user?.emailAddresses[0].emailAddress;
+  const username = user?.username;
+
+  const handleCreateUserProfile = async () => {
+    try {
+      if (email && signUpWithProvider) {
+        await signUpWithProvider({
+          email,
+          username,
+          provider: "clerk",
+          providerId: userId!,
+        });
+
+        navigate("/dashboard");
+      }
+    } catch (error) {
+      setErrors(["failed to signup with provider"]);
+    }
+  };
+
+  useEffect(() => {
+    handleCreateUserProfile();
+  }, []);
+  useEffect(() => {
+    console.log("userInfo", userInfo);
+  }, [userInfo]);
+
+  return (
+    <div className="h-[500px] w-full flex justify-center items-center">
+      <span>Loading your profile</span>
+      <span className="loading loading-spinner loading-lg"></span>
+    </div>
+  );
+}
