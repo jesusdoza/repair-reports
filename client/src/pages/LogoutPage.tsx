ef7f27c4277b32f29eca58d89c78045dd7e8e7f3
jesus doza <jesus.doza@gmail.com>
2024-09-16 10:52:44 -0500

    Fix logout page not redirecting to home

--- client/src/pages/LogoutPage.tsx
+++ client/src/pages/LogoutPage.tsx
@@ -1,12 +1,22 @@
+import { useEffect } from "react";
 import useAuthContext from "../hooks/useAuthContext";
 import { useAuth } from "@clerk/clerk-react";
+import { useNavigate } from "react-router-dom";
 
 export default function LogoutPage() {
   const { logout } = useAuthContext();
   const { signOut } = useAuth();
+  const navigate = useNavigate();
+  useEffect(() => {
+    const handleLogout = async () => {
+      await signOut();
+      if (logout) await logout();
 
-  signOut();
-  logout && logout();
+      navigate("/");
+    };
+
+    handleLogout();
+  }, []);
 
   return <div>LogoutPage</div>;
 }
