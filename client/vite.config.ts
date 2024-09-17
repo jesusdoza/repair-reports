0d4d70fa84c61d73f48c68d8d58dbdaae6001b04
jesus doza <jesus.doza@gmail.com>
2024-09-16 20:05:06 -0500

    FIX base route to vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV == "production" ? "/react" : "/",
+  base: process.env.NODE_ENV == "production" ? "/" : "/",
   // build: { outDir: "../backend/public/react", emptyOutDir: true },
 });
