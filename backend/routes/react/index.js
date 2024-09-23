04ff517eb4883f19bc140834e3ff6ee23a83a46e
Jesus Mendoza <101015821+jesusdoza@users.noreply.github.com>
2024-09-23 10:52:40 -0500

    FIX static react index file not serving up after clerk signin (#369)
    * FIX static react index file not serving up after clerk signin
    
    * FIX-backend server tests
    
    * FIX github action index serve test

--- backend/routes/react/index.js
+++ backend/routes/react/index.js
@@ -4,19 +4,24 @@ const path = require("path");
 const fs = require("fs");
 
 router.get("/", async (req, res) => {
-  console.log("getting page for react");
+  // console.log("getting page for react");
   const directoryPath = "../backend/public"; // Replace this with your directory path
 
   try {
     // Read the contents of the directory
     fs.readdir(directoryPath, (err, files) => {
+      // console.log("directoryPath", directoryPath);
+      // console.log("files", files);
       if (err) {
         console.error("Error reading directory:", err);
         return;
       }
     });
 
-    await fs.readFile(path.resolve("./public/index.html"));
+    console.log(
+      'path.resolve("./public/index.html")',
+      path.resolve("./public/index.html")
+    );
 
     res.sendFile(path.resolve("./public/index.html"));
   } catch (error) {
