7ce94aa8e8d648325135faa5a45064d01f2ed607
jesus doza <jesus.doza@gmail.com>
2025-03-25 21:32:04 -0500

    add shadcn

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import path from "path";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
@@ -6,4 +7,9 @@ export default defineConfig({
   plugins: [react()],
   base: process.env.NODE_ENV == "production" ? "/" : "/",
   // build: { outDir: "../backend/public/react", emptyOutDir: true },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
