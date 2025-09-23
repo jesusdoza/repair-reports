ac1306361aa01726961469394c347741ebf32aab
jesus doza <jesus.doza@gmail.com>
2025-09-23 09:38:32 -0500

    add express types

---
+++ backend/src/types/express.d.ts
@@ -0,0 +1,19 @@
+import { Request } from "express";
+import type { AuthObject } from "@clerk/express";
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: {
+        userId?: string; //auth provider user id
+        email?: string;
+        provider?: string;
+        emailVerified?: boolean;
+        appUserId?: string;
+      };
+      auth?: () => Promise<AuthObject>; // clerk middleware will add if user session exists
+    }
+  }
+}
+
+export {};
