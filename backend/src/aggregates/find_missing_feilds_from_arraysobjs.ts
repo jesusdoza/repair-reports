ef571050925273f95ba3177db0fa05aab973d5cc
jesus doza <jesus.doza@gmail.com>
2025-11-25 10:05:44 -0600

    add missing field aggregate

---
+++ backend/src/aggregates/find_missing_feilds_from_arraysobjs.ts
@@ -0,0 +1,22 @@
+//finding out if procedures array has any objects missing imageObjs field
+
+[
+  {
+    $match:
+      /**
+       * query: The query in MQL.
+       */
+      {
+        //on procerdures array, find objects missing imageObjs field
+        procedures: {
+          //match any
+          $elemMatch: {
+            //where imageObjs field does not exist
+            imageObjs: {
+              $exists: false,
+            },
+          },
+        },
+      },
+  },
+];
