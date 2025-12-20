95fd684d134095a25f8cd1b7384078cc6d758cbb
jesus doza <jesus.doza@gmail.com>
2025-12-19 20:05:20 -0600

    clean up profile page

--- client/src/components/Profile/UserInfoContainer.tsx
+++
@@ -1,6 +0,0 @@
-import UserInforUpdateForm from "./ProfileUpdate/UserInfoUpdateForm";
-
-export default function UserInfoContainer() {
-  //todo get users profile
-  return <UserInforUpdateForm />;
-}
