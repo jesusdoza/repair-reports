bc08f8a44224d921465e38c7c7cc6013d9bba1b5
jesus doza <jesus.doza@gmail.com>
2026-01-14 19:13:23 -0600

    debug repair data

--- client/src/pages/dashboard/UsersRepairs.tsx
+++ client/src/pages/dashboard/UsersRepairs.tsx
@@ -56,10 +56,10 @@ function RepairsList({
   //create react components
   const list = repairList.map((repairObj) => {
     const title = repairObj?.title || "untitled repair";
-
+    console.log("Repair Data: ", repairObj);
     const url =
       repairObj?.procedures && repairObj?.procedures.length > 0
-        ? repairObj?.procedures[0]?.images[0]
+        ? repairObj?.procedures[0]?.images[0].url
         : "#";
 
     // const summary = repairObj?.procedures[0]?.instructions
