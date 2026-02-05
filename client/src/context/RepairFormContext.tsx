28b4e25b9b2aa76ee02ec1287730a922956e13f0
jesus doza <jesus.doza@gmail.com>
2026-02-04 20:50:13 -0600

    reset repair form context

--- client/src/context/RepairFormContext.tsx
+++ client/src/context/RepairFormContext.tsx
@@ -1,326 +1,326 @@
-import { createContext, useState } from "react";
-import { Repair } from "../classes/Repair";
-import { addItem } from "../hooks/utils/addItem";
-import { Procedure } from "../classes/Procedure";
-import { ImageObj } from "../classes/ImageObj";
+// import { createContext, useState } from "react";
+// import { Repair } from "../classes/Repair";
+// import { addItem } from "../hooks/utils/addItem";
+// import { Procedure } from "../classes/Procedure";
+// import { RepairImage } from "../classes/RepairImage";
 
-export type formActionT = {
-  addProcedureAfter: (id: string, item: Procedure) => void;
-  addProcedureAtBegining: (item: Procedure) => void;
-  removeProcedure: (id: string) => void;
-  updateInstructions: (id: string, text: string) => void;
-  updateTitle: (title: string) => void;
-  updateEngineMake: (title: string) => void;
-  updateGroup: (title: string) => void;
-  updateBoardType: (title: string) => void;
-  addImage: (item: ImageObj, procedureId: string) => void;
-  updateImage: (item: ImageObj, procedureId: string) => void;
-  removeImage: (imageId: string, procedureId: string) => void;
-  replaceImageObjs: (list: ImageObj[], procedureId: string) => void;
-};
+// export type formActionT = {
+//   addProcedureAfter: (id: string, item: Procedure) => void;
+//   addProcedureAtBegining: (item: Procedure) => void;
+//   removeProcedure: (id: string) => void;
+//   updateInstructions: (id: string, text: string) => void;
+//   updateTitle: (title: string) => void;
+//   updateEngineMake: (title: string) => void;
+//   updateGroup: (title: string) => void;
+//   updateBoardType: (title: string) => void;
+//   addImage: (item: RepairImage, procedureId: string) => void;
+//   updateImage: (item: RepairImage, procedureId: string) => void;
+//   removeImage: (imageId: string, procedureId: string) => void;
+//   replaceImageObjs: (list: RepairImage[], procedureId: string) => void;
+// };
 
-export type RepairFormDataContextT = {
-  repairFormData: Repair;
-  initializeRepairFormData: (repair: Repair) => void;
-  formAction: {
-    addProcedureAfter: (id: string, item: Procedure) => void;
-    removeProcedure: (id: string) => void;
-    addProcedureAtBegining: (item: Procedure) => void;
-    updateInstructions: (id: string, text: string) => void;
-    updateTitle: (title: string) => void;
-    updateEngineMake: (title: string) => void;
-    updateGroup: (title: string) => void;
-    updateBoardType: (title: string) => void;
-    addImage: (item: ImageObj, procedureId: string) => void;
-    updateImage: (item: ImageObj, procedureId: string) => void;
-    removeImage: (imageId: string, procedureId: string) => void;
-    replaceImageObjs: (list: ImageObj[], procedureId: string) => void;
-  };
-};
+// export type RepairFormDataContextT = {
+//   repairFormData: Repair;
+//   initializeRepairFormData: (repair: Repair) => void;
+//   formAction: {
+//     addProcedureAfter: (id: string, item: Procedure) => void;
+//     removeProcedure: (id: string) => void;
+//     addProcedureAtBegining: (item: Procedure) => void;
+//     updateInstructions: (id: string, text: string) => void;
+//     updateTitle: (title: string) => void;
+//     updateEngineMake: (title: string) => void;
+//     updateGroup: (title: string) => void;
+//     updateBoardType: (title: string) => void;
+//     addImage: (item: RepairImage, procedureId: string) => void;
+//     updateImage: (item: RepairImage, procedureId: string) => void;
+//     removeImage: (imageId: string, procedureId: string) => void;
+//     replaceImageObjs: (list: RepairImage[], procedureId: string) => void;
+//   };
+// };
 
-// const repairForm = new Repair();
+// // const repairForm = new Repair();
 
-export const RepairFormDataContext = createContext<RepairFormDataContextT>({
-  repairFormData: new Repair(),
-  initializeRepairFormData: () => {},
-  formAction: {
-    addProcedureAfter: () => {},
-    removeProcedure: () => {},
-    addProcedureAtBegining: () => {},
-    updateInstructions: () => {},
-    updateTitle: () => {},
-    updateEngineMake: () => {},
-    updateGroup: () => {},
-    updateBoardType: () => {},
-    addImage: () => {},
-    updateImage: () => {},
-    removeImage: () => {},
-    replaceImageObjs: () => {},
-  },
-});
+// export const RepairFormDataContext = createContext<RepairFormDataContextT>({
+//   repairFormData: new Repair(),
+//   initializeRepairFormData: () => {},
+//   formAction: {
+//     addProcedureAfter: () => {},
+//     removeProcedure: () => {},
+//     addProcedureAtBegining: () => {},
+//     updateInstructions: () => {},
+//     updateTitle: () => {},
+//     updateEngineMake: () => {},
+//     updateGroup: () => {},
+//     updateBoardType: () => {},
+//     addImage: () => {},
+//     updateImage: () => {},
+//     removeImage: () => {},
+//     replaceImageObjs: () => {},
+//   },
+// });
 
-//Provider
-export const RepairContextProvider = ({
-  children,
-}: {
-  children: React.ReactNode;
-}) => {
-  const repairForm = new Repair();
-  const [repairFormData, setRepairFormData] = useState(repairForm);
-  // const [repairFormData, setRepairFormData] = useState(repairForm);
+// //Provider
+// export const RepairContextProvider = ({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) => {
+//   const repairForm = new Repair();
+//   const [repairFormData, setRepairFormData] = useState(repairForm);
+//   // const [repairFormData, setRepairFormData] = useState(repairForm);
 
-  function updateTitle(title: string) {
-    setRepairFormData((state) => {
-      state.title = title;
-      return state;
-    });
-  }
-  /// add to beggining of procedure array
-  function addProcedureAtBegining(item: Procedure) {
-    //update procedures
-    const newArr = addItem({
-      pos: "begining",
-      arr: repairFormData.procedureArr,
-      item,
-    });
+//   function updateTitle(title: string) {
+//     setRepairFormData((state) => {
+//       state.title = title;
+//       return state;
+//     });
+//   }
+//   /// add to beggining of procedure array
+//   function addProcedureAtBegining(item: Procedure) {
+//     //update procedures
+//     const newArr = addItem({
+//       pos: "begining",
+//       arr: repairFormData.procedures,
+//       item,
+//     });
 
-    setRepairFormData((state) => {
-      state.procedureArr = newArr;
+//     setRepairFormData((state) => {
+//       state.procedures = newArr;
 
-      return state;
-    });
-  }
+//       return state;
+//     });
+//   }
 
-  function removeProcedure(id: string) {
-    //update procedures
-    setRepairFormData((state) => {
-      state.procedureArr = state.procedureArr.filter((proc) => proc._id != id);
-      return state;
-    });
-  }
+//   function removeProcedure(id: string) {
+//     //update procedures
+//     setRepairFormData((state) => {
+//       state.procedures = state.procedures.filter((proc) => proc._id != id);
+//       return state;
+//     });
+//   }
 
-  ///update insturctions
-  function updateInstructions(id: string, text: string) {
-    setRepairFormData((state) => {
-      const newArr = state.procedureArr.map((proc) => {
-        if (proc._id == id) {
-          proc.instructions = text;
-          return proc;
-        }
-        return proc;
-      });
-      state.procedureArr = newArr;
-      return state;
-    });
-  }
+//   ///update insturctions
+//   function updateInstructions(id: string, text: string) {
+//     setRepairFormData((state) => {
+//       const newArr = state.procedures.map((proc) => {
+//         if (proc._id == id) {
+//           proc.instructions = text;
+//           return proc;
+//         }
+//         return proc;
+//       });
+//       state.procedures = newArr;
+//       return state;
+//     });
+//   }
 
-  ///add procedure after the id provided
-  function addProcedureAfter(id: string, item: Procedure) {
-    if (repairFormData.procedureArr.length < 2) {
-      setRepairFormData((state) => {
-        state.procedureArr.push(item);
+//   ///add procedure after the id provided
+//   function addProcedureAfter(id: string, item: Procedure) {
+//     if (repairFormData.procedures.length < 2) {
+//       setRepairFormData((state) => {
+//         state.procedures.push(item);
 
-        return state;
-      });
-      return;
-    }
+//         return state;
+//       });
+//       return;
+//     }
 
-    const newArr = addItem({
-      id,
-      pos: "after",
-      arr: repairFormData.procedureArr,
-      item,
-    });
+//     const newArr = addItem({
+//       id,
+//       pos: "after",
+//       arr: repairFormData.procedures,
+//       item,
+//     });
 
-    setRepairFormData((state) => {
-      state.procedureArr = newArr;
+//     setRepairFormData((state) => {
+//       state.procedures = newArr;
 
-      return state;
-    });
-  }
+//       return state;
+//     });
+//   }
 
-  //initialize form data
-  function initializeRepairFormData(newRepair: Repair) {
-    // const newRepair = new Repair(repair);
+//   //initialize form data
+//   function initializeRepairFormData(newRepair: Repair) {
+//     // const newRepair = new Repair(repair);
 
-    // console.log("setRepairdata");
-    // console.log("newRepair", newRepair);
+//     // console.log("setRepairdata");
+//     // console.log("newRepair", newRepair);
 
-    setRepairFormData(newRepair);
-  }
+//     setRepairFormData(newRepair);
+//   }
 
-  function updateEngineMake(value: string) {
-    setRepairFormData((state) => {
-      state.engineMake = value;
-      return state;
-    });
-  }
+//   function updateEngineMake(value: string) {
+//     setRepairFormData((state) => {
+//       state.category = value;
+//       return state;
+//     });
+//   }
 
-  function updateGroup(value: string) {
-    setRepairFormData((state) => {
-      state.engineMake = value;
-      return state;
-    });
-  }
+//   function updateGroup(value: string) {
+//     setRepairFormData((state) => {
+//       state.category = value;
+//       return state;
+//     });
+//   }
 
-  function updateBoardType(value: string) {
-    setRepairFormData((state) => {
-      state.engineMake = value;
-      return state;
-    });
-  }
+//   function updateBoardType(value: string) {
+//     setRepairFormData((state) => {
+//       state.category = value;
+//       return state;
+//     });
+//   }
 
-  ///add image to procedure
-  function addImage(item: ImageObj, procedureId: string) {
-    const targetProcedure = repairFormData.procedureArr.findIndex((proc) => {
-      if (proc._id == procedureId) {
-        return true;
-      }
-      return false;
-    });
+//   ///add image to procedure
+//   function addImage(item: RepairImage, procedureId: string) {
+//     const targetProcedure = repairFormData.procedures.findIndex((proc) => {
+//       if (proc._id == procedureId) {
+//         return true;
+//       }
+//       return false;
+//     });
 
-    const targetProcedureData = repairFormData.procedureArr[targetProcedure];
-    const imageObjArr = targetProcedureData.imageObjs;
+//     const targetProcedureData = repairFormData.procedures[targetProcedure];
+//     const imageObjArr = targetProcedureData.images;
 
-    const newImageObjs = addItem({
-      pos: "end",
-      arr: imageObjArr,
-      item,
-    });
+//     const newImageObjs = addItem({
+//       pos: "end",
+//       arr: imageObjArr,
+//       item,
+//     });
 
-    //updating older implementation for backward compatibility to EJS app
-    const imageStringsArr = newImageObjs.map((data) => data.imageUrl);
+//     //updating older implementation for backward compatibility to EJS app
+//     const imageStringsArr = newImageObjs.map((data) => data.url);
 
-    setRepairFormData((state) => {
-      const targetProc = state.procedureArr[targetProcedure];
-      targetProc.imageObjs = newImageObjs;
-      targetProc.images = imageStringsArr;
+//     setRepairFormData((state) => {
+//       const targetProc = state.procedures[targetProcedure];
+//       targetProc.images = newImageObjs;
+//       targetProc.images = imageStringsArr;
 
-      return state;
-    });
-  }
-  function replaceImageObjs(list: ImageObj[], procedureId: string) {
-    const targetProcedure = repairFormData.procedureArr.findIndex((proc) => {
-      if (proc._id == procedureId) {
-        return true;
-      }
-      return false;
-    });
+//       return state;
+//     });
+//   }
+//   function replaceImageObjs(list: RepairImage[], procedureId: string) {
+//     const targetProcedure = repairFormData.procedures.findIndex((proc) => {
+//       if (proc._id == procedureId) {
+//         return true;
+//       }
+//       return false;
+//     });
 
-    // const targetProcedureData = repairFormData.procedureArr[targetProcedure];
+//     // const targetProcedureData = repairFormData.procedureArr[targetProcedure];
 
-    const newImageObjs = list;
+//     const newImageObjs = list;
 
-    //updating older implementation for backward compatibility to EJS app
-    const imageStringsArr = newImageObjs.map((data) => data.imageUrl);
+//     //updating older implementation for backward compatibility to EJS app
+//     const imageStringsArr = newImageObjs.map((data) => data.url);
 
-    setRepairFormData((state) => {
-      const targetProc = state.procedureArr[targetProcedure];
-      targetProc.imageObjs = newImageObjs;
-      targetProc.images = imageStringsArr;
+//     setRepairFormData((state) => {
+//       const targetProc = state.procedures[targetProcedure];
+//       targetProc.images = newImageObjs;
+//       targetProc.images = imageStringsArr;
 
-      return state;
-    });
-  }
+//       return state;
+//     });
+//   }
 
-  //remove by id
-  function removeImage(imageId: string, procedureId: string) {
-    //get target procedure data
-    const targetProcedure = repairFormData.procedureArr.findIndex((proc) => {
-      if (proc._id == procedureId) {
-        return true;
-      }
-      return false;
-    });
+//   //remove by id
+//   function removeImage(imageId: string, procedureId: string) {
+//     //get target procedure data
+//     const targetProcedure = repairFormData.procedures.findIndex((proc) => {
+//       if (proc._id == procedureId) {
+//         return true;
+//       }
+//       return false;
+//     });
 
-    //filter out target id and return wanted imageObjs
-    const newImageObjs = repairFormData.procedureArr[
-      targetProcedure
-    ].imageObjs.filter((imageData) => {
-      if (imageData._id == imageId) return false;
+//     //filter out target id and return wanted imageObjs
+//     const newImageObjs = repairFormData.procedures[
+//       targetProcedure
+//     ].images.filter((imageData) => {
+//       if (imageData._id == imageId) return false;
 
-      return true;
-    });
+//       return true;
+//     });
 
-    //updating older implementation for backward compatibility to EJS app
-    const imageStringsArr = newImageObjs.map((data) => data.imageUrl);
+//     //updating older implementation for backward compatibility to EJS app
+//     const imageStringsArr = newImageObjs.map((data) => data.url);
 
-    //update form data state without causing rerender
-    setRepairFormData((state) => {
-      const targetProc = state.procedureArr[targetProcedure];
-      targetProc.imageObjs = newImageObjs;
-      targetProc.images = imageStringsArr;
+//     //update form data state without causing rerender
+//     setRepairFormData((state) => {
+//       const targetProc = state.procedures[targetProcedure];
+//       targetProc.images = newImageObjs;
+//       targetProc.images = imageStringsArr;
 
-      return state;
-    });
-  }
+//       return state;
+//     });
+//   }
 
-  //update image data by id
-  function updateImage(newImageData: ImageObj, procedureId: string) {
-    console.log("newImageData", newImageData);
-    console.log("procedureId", procedureId);
-    const targetProcedure = repairFormData.procedureArr.findIndex((proc) => {
-      if (proc._id == procedureId) {
-        return true;
-      }
-      return false;
-    });
+//   //update image data by id
+//   function updateImage(newImageData: RepairImage, procedureId: string) {
+//     console.log("newImageData", newImageData);
+//     console.log("procedureId", procedureId);
+//     const targetProcedure = repairFormData.procedures.findIndex((proc) => {
+//       if (proc._id == procedureId) {
+//         return true;
+//       }
+//       return false;
+//     });
 
-    //get images Data array from target procedure
-    const targetProcedureData = repairFormData.procedureArr[targetProcedure];
-    const imageObjsArr = targetProcedureData.imageObjs;
+//     //get images Data array from target procedure
+//     const targetProcedureData = repairFormData.procedures[targetProcedure];
+//     const imageObjsArr = targetProcedureData.images;
 
-    //replace target image data by id
-    const newImageObjs = imageObjsArr.map((imageData) => {
-      if (imageData._id == newImageData._id) {
-        return newImageData;
-      }
-      return imageData;
-    });
+//     //replace target image data by id
+//     const newImageObjs = imageObjsArr.map((imageData) => {
+//       if (imageData._id == newImageData._id) {
+//         return newImageData;
+//       }
+//       return imageData;
+//     });
 
-    //updating older implementation for backward compatibility to EJS app
-    const imageStringsArr = newImageObjs.map((data) => data.imageUrl);
+//     //updating older implementation for backward compatibility to EJS app
+//     const imageStringsArr = newImageObjs.map((data) => data.url);
 
-    console.log("newImageObjs", newImageObjs);
+//     console.log("newImageObjs", newImageObjs);
 
-    //update form state
-    setRepairFormData((state) => {
-      const targetProc = state.procedureArr[targetProcedure];
-      targetProc.imageObjs = newImageObjs;
-      targetProc.images = imageStringsArr;
+//     //update form state
+//     setRepairFormData((state) => {
+//       const targetProc = state.procedures[targetProcedure];
+//       targetProc.images = newImageObjs;
+//       targetProc.images = imageStringsArr;
 
-      return state;
-    });
-  }
-  ///values to set in context
-  const values = {
-    repairFormData,
-    initializeRepairFormData,
-    formAction: {
-      updateEngineMake,
-      updateBoardType,
-      updateGroup,
-      addProcedureAfter,
-      removeProcedure,
-      addProcedureAtBegining,
-      updateInstructions,
-      updateTitle,
-      addImage,
-      removeImage,
-      updateImage,
-      replaceImageObjs,
-    },
-  };
+//       return state;
+//     });
+//   }
+//   ///values to set in context
+//   const values = {
+//     repairFormData,
+//     initializeRepairFormData,
+//     formAction: {
+//       updateEngineMake,
+//       updateBoardType,
+//       updateGroup,
+//       addProcedureAfter,
+//       removeProcedure,
+//       addProcedureAtBegining,
+//       updateInstructions,
+//       updateTitle,
+//       addImage,
+//       removeImage,
+//       updateImage,
+//       replaceImageObjs,
+//     },
+//   };
 
-  return (
-    <RepairFormDataContext.Provider value={values}>
-      {children}
-    </RepairFormDataContext.Provider>
-  );
-};
+//   return (
+//     <RepairFormDataContext.Provider value={values}>
+//       {children}
+//     </RepairFormDataContext.Provider>
+//   );
+// };
 
-export function changeTitle(repair: Repair, newTitle: string) {
-  repair.title = newTitle;
+// export function changeTitle(repair: Repair, newTitle: string) {
+//   repair.title = newTitle;
 
-  return repair;
-}
+//   return repair;
+// }
