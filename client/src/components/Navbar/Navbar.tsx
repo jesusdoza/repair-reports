f8665b19d40c3616d4a483b777365b349e3683ef
jesus doza <jesus.doza@gmail.com>
2026-02-06 10:55:30 -0600

    refactor nav to use clerk data

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,8 +1,7 @@
 import { Link } from "react-router-dom";
 import Search from "../Search/Search";
-import useAuthContext from "../../hooks/useAuthContext";
 import { v4 } from "uuid";
-import { useAuth, useUser } from "@clerk/clerk-react";
+import { useUser } from "@clerk/clerk-react";
 
 export type NavLinkT = {
   url: string;
@@ -19,7 +18,7 @@ function MenuItem({ label, url }: NavLinkT) {
   return (
     <Link
       to={url}
-      className="bg-slate-300">
+      className="">
       <span className="text-black">{label}</span>
     </Link>
   );
@@ -29,7 +28,9 @@ const NavbarMenu = ({ mainMenu }: { mainMenu: NavLinkT[] }) => {
   if (!mainMenu || mainMenu.length === 0) return null;
   const items = mainMenu.map((item) => {
     return (
-      <li key={v4()}>
+      <li
+        className=""
+        key={v4()}>
         <MenuItem
           label={item.label}
           url={item.url}
@@ -38,7 +39,11 @@ const NavbarMenu = ({ mainMenu }: { mainMenu: NavLinkT[] }) => {
     );
   });
 
-  return <ul>{items}</ul>;
+  return (
+    <ul className="menu menu-vertical lg:menu-horizontal bg-secondary rounded-box ">
+      {items}
+    </ul>
+  );
 };
 
 export default function Navbar({
@@ -80,17 +85,13 @@ export default function Navbar({
         </ul>
       </div> */}
       <div>
-        <div className="navbar bg-base-100 shadow-sm">
+        <div className="navbar bg-secondary shadow-sm">
           <div className="flex-1">
             <a className="btn btn-ghost text-xl">daisyUI</a>
           </div>
           <NavbarMenu mainMenu={mainMenu} />
           <div className="flex gap-2">
-            <input
-              type="text"
-              placeholder="Search"
-              className="input input-bordered w-24 md:w-auto"
-            />
+            <Search />
             <div className="dropdown dropdown-end">
               <div
                 tabIndex={0}
