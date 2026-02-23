25d6018f90e21aa89972f53f3d30e587648b0888
jesus doza <jesus.doza@gmail.com>
2026-02-23 09:00:28 -0600

    change color scheme

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,25 +5,12 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
       colors: {
-        text: {
-          primary: "var(--text-primary)",
-        },
+        border: "var(--border)",
+        input: "var(--input)",
+        ring: "var(--ring)",
         background: "var(--background)",
         foreground: "var(--foreground)",
-        card: {
-          DEFAULT: "var(--card)",
-          foreground: "var(--card-foreground)",
-        },
-        popover: {
-          DEFAULT: "var(--popover)",
-          foreground: "var(--popover-foreground)",
-        },
         primary: {
           DEFAULT: "var(--primary)",
           foreground: "var(--primary-foreground)",
@@ -31,6 +18,10 @@ export default {
         secondary: {
           DEFAULT: "var(--secondary)",
           foreground: "var(--secondary-foreground)",
+        },
+        destructive: {
+          DEFAULT: "var(--destructive)",
+          foreground: "var(--destructive-foreground)",
         },
         muted: {
           DEFAULT: "var(--muted)",
@@ -40,13 +31,14 @@ export default {
           DEFAULT: "var(--accent)",
           foreground: "var(--accent-foreground)",
         },
-        destructive: {
-          DEFAULT: "var(--destructive)",
-          foreground: "var(--destructive-foreground)",
+        popover: {
+          DEFAULT: "var(--popover)",
+          foreground: "var(--popover-foreground)",
         },
-        border: "var(--border)",
-        input: "var(--input)",
-        ring: "var(--ring)",
+        card: {
+          DEFAULT: "var(--card)",
+          foreground: "var(--card-foreground)",
+        },
         chart: {
           1: "var(--chart-1)",
           2: "var(--chart-2)",
@@ -54,8 +46,68 @@ export default {
           4: "var(--chart-4)",
           5: "var(--chart-5)",
         },
+        text: {
+          primary: "var(--text-primary)",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
     },
+    // extend: {
+    //   borderRadius: {
+    //     lg: "var(--radius)",
+    //     md: "calc(var(--radius) - 2px)",
+    //     sm: "calc(var(--radius) - 4px)",
+    //   },
+    //   colors: {
+    //     text: {
+    //       primary: "var(--text-primary)",
+    //     },
+    //     background: "var(--background)",
+    //     foreground: "var(--foreground)",
+    //     card: {
+    //       DEFAULT: "var(--card)",
+    //       foreground: "var(--card-foreground)",
+    //     },
+    //     popover: {
+    //       DEFAULT: "var(--popover)",
+    //       foreground: "var(--popover-foreground)",
+    //     },
+    //     primary: {
+    //       DEFAULT: "var(--primary)",
+    //       foreground: "var(--primary-foreground)",
+    //     },
+    //     secondary: {
+    //       DEFAULT: "var(--secondary)",
+    //       foreground: "var(--secondary-foreground)",
+    //     },
+    //     muted: {
+    //       DEFAULT: "var(--muted)",
+    //       foreground: "var(--muted-foreground)",
+    //     },
+    //     accent: {
+    //       DEFAULT: "var(--accent)",
+    //       foreground: "var(--accent-foreground)",
+    //     },
+    //     destructive: {
+    //       DEFAULT: "var(--destructive)",
+    //       foreground: "var(--destructive-foreground)",
+    //     },
+    //     border: "var(--border)",
+    //     input: "var(--input)",
+    //     ring: "var(--ring)",
+    //     chart: {
+    //       1: "var(--chart-1)",
+    //       2: "var(--chart-2)",
+    //       3: "var(--chart-3)",
+    //       4: "var(--chart-4)",
+    //       5: "var(--chart-5)",
+    //     },
+    //   },
+    // },
   },
   plugins: [daisyui, import("tailwindcss-animate")],
 };
